a6e373bc2540e02e444adb78c2a61843c5a4d5bc
Roshan Raghupathy <thisisroshansmail@gmail.com>
2019-04-12 18:29:40 +0530

    Message forwarding and deduplication. Per stream read buffers.

--- examples/client.cpp
+++ examples/client.cpp
@@ -15,8 +15,16 @@ using namespace std;
 
 class PubSubClient {
 public:
-	void did_receive_message(std::unique_ptr<char[]> &&message, uint64_t size, std::string &channel) {
-		spdlog::info("Received message on channel {}: {}", channel, spdlog::to_hex(message.get(), message.get() + size));
+	void did_unsubscribe(PubSubNode<PubSubClient> &, std::string channel) {
+		spdlog::info("Did unsubscribe: {}", channel);
+	}
+
+	void did_subscribe(PubSubNode<PubSubClient> &, std::string channel) {
+		spdlog::info("Did subscribe: {}", channel);
+	}
+
+	void did_receive_message(PubSubNode<PubSubClient> &, std::unique_ptr<char[]> &&message, uint64_t size, std::string &channel, uint64_t message_id) {
+		spdlog::info("Received message {} on channel {}: {}", message_id, channel, spdlog::to_hex(message.get(), message.get() + size));
 	}
 };
 
@@ -29,29 +37,22 @@ int main() {
 
 	auto addr = SocketAddress::from_string("127.0.0.1:8000");
 	auto b = new NodeType(addr);
-	b->delegate = b_del;
+	b->delegate = &b_del;
 	b->start_listening();
 
 	auto addr2 = SocketAddress::from_string("127.0.0.1:8001");
 	auto b2 = new NodeType(addr2);
-	b2->delegate = b2_del;
+	b2->delegate = &b2_del;
 	b2->start_listening();
-
-	#define SIZE 12500
-
-	std::unique_ptr<char[]> data(new char[SIZE]);
-	fill(data.get(), data.get()+SIZE, 'B');
 
 	spdlog::info("Start");
 
 	// b->send_MESSAGE(addr2, std::string("Test channel"), data.get(), SIZE);
 
 	b->send_SUBSCRIBE(addr2, std::string("test_channel"));
-	b2->send_SUBSCRIBE(addr2, std::string("test_channel"));
+	// b2->send_SUBSCRIBE(addr2, std::string("test_channel"));
 
-	b->send_UNSUBSCRIBE(addr2, std::string("test_channel"));
-
-	b2->send_message_on_channel(std::string("test_channel"), data.get(), SIZE);
+	// b->send_UNSUBSCRIBE(addr2, std::string("test_channel"));
 
 	return uv_run(uv_default_loop(), UV_RUN_DEFAULT);
 }
