5af0a69ba4e5e0386c3950f136c846bc843162ad
amolfound <amol>
2019-08-30 12:29:55 +0530

    added churn algorithm

--- include/OnRampPubSub.hpp
+++ include/OnRampPubSub.hpp
@@ -10,7 +10,7 @@ public:
 	OnRampPubSub(const net::SocketAddress &addr) : PubSubNode<PubSubDelegate>(addr) {};
 
 protected:
-	void ManageSubscribers();
+	void manage_subscribers();
 };
 
 /* TODO:
@@ -20,7 +20,7 @@ protected:
 	Put maxSubsriptions in a config
 */
 template<typename PubSubDelegate>
-void OnRampPubSub<PubSubDelegate>::ManageSubscribers() {
+void OnRampPubSub<PubSubDelegate>::manage_subscribers() {
 
 	SPDLOG_DEBUG("Managing peers");
 
@@ -29,13 +29,33 @@ void OnRampPubSub<PubSubDelegate>::ManageSubscribers() {
 		this->delegate->channels.end(),
 		[&] (std::string const channel) {
 
+			// create alias not copy	
+			typename PubSubNode<PubSubDelegate>::TransportSet& temp_transport_set = this->channel_subscriptions[channel];
+			typename PubSubNode<PubSubDelegate>::TransportSet& temp_potential_transport_set = this->channel_subscriptions[channel];
+
+			typename PubSubNode<PubSubDelegate>::BaseTransport* ToReplaceTransport;
+			typename PubSubNode<PubSubDelegate>::BaseTransport* ToReplaceWithTransport;
+
 			// move some of the subscribers to potential subscribers if oversubscribed
-			if (this->channel_subscriptions[channel].size() > DefaultMaxSubscriptions) {
+			if (temp_transport_set.size() > DefaultMaxSubscriptions) {
 				// insert churn algorithm here
 				// send message to removed and added peers
+
+				ToReplaceTransport = this->find_min_rtt_transport(temp_transport_set);
+				ToReplaceWithTransport = this->find_max_rtt_transport(temp_potential_transport_set);
+
+				if (ToReplaceTransport != NULL &&
+					ToReplaceWithTransport != NULL &&
+					ToReplaceTransport->get_rtt() > ToReplaceWithTransport->get_rtt()) {
+
+					temp_transport_set.erase(ToReplaceTransport);
+					temp_transport_set.insert(ToReplaceWithTransport);
+					temp_potential_transport_set.erase(ToReplaceWithTransport);
+					temp_potential_transport_set.insert(ToReplaceTransport);
+				}
 			}
 
-			for (auto* pot_transport :this-> potential_channel_subscriptions[channel]) {
+			for (auto* pot_transport : temp_potential_transport_set) {
 				// add condition to check if rtt is too old, ideally this should be job transport manager?
 				// send dummy packet to estimate new rtt
 				SPDLOG_DEBUG("Channel: {} rtt: {}", channel, pot_transport->get_rtt());
