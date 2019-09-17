6c9eb175c97565f244110a4c39d81f4baab83d51
Roshan Raghupathy <thisisroshansmail@gmail.com>
2019-09-17 20:23:54 +0530

    Autoresolve dependencies

---
+++ cmake/spdlog.cmake
@@ -0,0 +1,18 @@
+find_package(spdlog NO_CMAKE_PACKAGE_REGISTRY)
+if(NOT spdlog_FOUND)
+	message("Using internal spdlog")
+
+	include(FetchContent)
+
+	FetchContent_Declare(spdlog
+		GIT_REPOSITORY https://github.com/gabime/spdlog.git
+		GIT_TAG v1.3.1
+	)
+	FetchContent_MakeAvailable(spdlog)
+
+	install(TARGETS spdlog
+		EXPORT marlin-net-export
+		LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
+		ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR}
+	)
+endif()
