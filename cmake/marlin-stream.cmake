a14d397883efa9cb3b56e56e4c8a6e61baaa9aa3
amolfound <amol>
2019-09-18 13:19:24 +0530

    Auto resolve dependencies

---
+++ cmake/marlin-stream.cmake
@@ -0,0 +1,12 @@
+find_package(MarlinStream)
+if(NOT MarlinStream_FOUND)
+	message("Using internal MarlinStream")
+	include(FetchContent)
+	FetchContent_Declare(MarlinStream
+		GIT_REPOSITORY https://gitlab.com/marlinprotocol/stream.cpp.git
+		GIT_TAG amol/deps
+		# to be changed when merged
+		# GIT_TAG master
+	)
+	FetchContent_MakeAvailable(MarlinStream)	
+endif()
