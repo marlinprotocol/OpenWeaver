e1e055841b3581f75cb50e0fe7af442ff05f49f0
Amol Agrawal <amolfound@gmail.com>
2019-09-19 21:58:51 +0530

    changing folder structure and auto resolving depend...

---
+++ cmake/snappy.cmake
@@ -0,0 +1,14 @@
+find_package(snappy NO_CMAKE_PACKAGE_REGISTRY)
+if(NOT snappy_FOUND)
+	message("Using internal snappy")
+
+	include(FetchContent)
+
+	FetchContent_Declare(snappy
+		GIT_REPOSITORY https://github.com/google/snappy.git
+		GIT_TAG 1.1.7
+	)
+	FetchContent_MakeAvailable(snappy)
+
+	add_library(Snappy::snappy ALIAS snappy)
+endif()
