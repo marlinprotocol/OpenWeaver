134f7b834396ecba3603a2dea6b5b9e1c6b5e795
Roshan Raghupathy <thisisroshansmail@gmail.com>
2019-09-21 12:40:08 +0530

    CMake streamlining

--- cmake/marlin-pubsub.cmake
+++ cmake/marlin-pubsub.cmake
@@ -1,6 +1,6 @@
-find_package(MarlinPubsub)
+find_package(MarlinPubsub QUIET)
 if(NOT MarlinPubsub_FOUND)
-    message("Using internal MarlinPubsub")
+	message("-- MarlinPubsub not found. Using internal MarlinPubsub.")
     include(FetchContent)
     FetchContent_Declare(MarlinPubsub
         GIT_REPOSITORY https://gitlab.com/marlinprotocol/pubsub.cpp.git
@@ -9,4 +9,6 @@ if(NOT MarlinPubsub_FOUND)
     FetchContent_MakeAvailable(MarlinPubsub)
 
     add_library(Marlin::pubsub ALIAS pubsub)
+else()
+	message("-- MarlinPubsub found. Using system MarlinPubsub.")
 endif()
