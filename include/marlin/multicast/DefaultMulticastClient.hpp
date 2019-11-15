ba136eee66e66d5c5043d81d40c916d48b4e8156
Roshan Raghupathy <thisisroshansmail@gmail.com>
2019-11-15 10:22:46 +0530

    Initial commit

---
+++ include/marlin/multicast/DefaultMulticastClient.hpp
@@ -0,0 +1,150 @@
+#ifndef MARLIN_MULTICAST_DEFAULTMULTICASTCLIENT_HPP
+#define MARLIN_MULTICAST_DEFAULTMULTICASTCLIENT_HPP
+
+#include <marlin/pubsub/PubSubClient.hpp>
+#include <marlin/beacon/DiscoveryClient.hpp>
+#include <uv.h>
+
+
+namespace marlin {
+namespace multicast {
+
+#define PUBSUB_PROTOCOL_NUMBER 0x10000000
+
+struct DefaultMulticastClientOptions {
+	std::vector<std::string> channels = {"default"};
+	std::string beacon_addr = "127.0.0.1:9002";
+	std::string discovery_addr = "127.0.0.1:8002";
+	std::string pubsub_addr = "127.0.0.1:8000";
+};
+
+template<typename Delegate>
+class DefaultMulticastClient {
+public:
+	using PubSubClientType = pubsub::PubSubClient<DefaultMulticastClient<Delegate>>;
+
+	beacon::DiscoveryClient<DefaultMulticastClient<Delegate>> b;
+	PubSubClientType ps;
+
+	std::vector<std::string> channels = {"default"};
+
+	Delegate *delegate = nullptr;
+
+	void new_peer(
+		net::SocketAddress const &addr,
+		uint32_t protocol,
+		uint16_t
+	) {
+		if(protocol == PUBSUB_PROTOCOL_NUMBER) {
+			ps.subscribe(addr);
+
+			// TODO: Better design
+			ps.add_subscriber(addr);
+		}
+	}
+
+	std::vector<std::tuple<uint32_t, uint16_t, uint16_t>> get_protocols() {
+		return {};
+	}
+
+	void did_unsubscribe(
+		PubSubClientType &,
+		std::string channel __attribute__((unused))
+	) {
+		SPDLOG_DEBUG("Did unsubscribe: {}", channel);
+		delegate->did_unsubscribe(*this, channel);
+	}
+
+	void did_subscribe(
+		PubSubClientType &,
+		std::string channel __attribute__((unused))
+	) {
+		SPDLOG_DEBUG("Did subscribe: {}", channel);
+		delegate->did_subscribe(*this, channel);
+	}
+
+	void did_recv_message(
+		PubSubClientType &,
+		net::Buffer &&message,
+		std::string &channel __attribute__((unused)),
+		uint64_t message_id __attribute__((unused))
+	) {
+		SPDLOG_DEBUG(
+			"Received message {} on channel {}",
+			message_id,
+			channel
+		);
+		delegate->did_recv_message(*this, std::move(message), channel, message_id);
+	}
+
+	void manage_subscribers(
+		std::string channel,
+		typename PubSubClientType::TransportSet& transport_set,
+		typename PubSubClientType::TransportSet& potential_transport_set
+	) {
+		// move some of the subscribers to potential subscribers if oversubscribed
+		if (transport_set.size() >= DefaultMaxSubscriptions) {
+			// insert churn algorithm here. need to find a better algorithm to give old bad performers a chance gain. Pick randomly from potential peers?
+			// send message to removed and added peers
+
+			auto* toReplaceTransport = transport_set.find_max_rtt_transport();
+			auto* toReplaceWithTransport = potential_transport_set.find_random_rtt_transport();
+
+			if (toReplaceTransport != nullptr &&
+				toReplaceWithTransport != nullptr) {
+
+				SPDLOG_INFO(
+					"Moving address: {} from subscribers to potential subscribers list on channel: {} ",
+					toReplaceTransport->dst_addr.to_string(),
+					channel
+				);
+
+				ps.remove_subscriber_from_channel(channel, *toReplaceTransport);
+				ps.add_subscriber_to_potential_channel(channel, *toReplaceTransport);
+
+				SPDLOG_INFO(
+					"Moving address: {} from potential subscribers to subscribers list on channel: {} ",
+					toReplaceWithTransport->dst_addr.to_string(),
+					channel
+				);
+
+				ps.remove_subscriber_from_potential_channel(channel, *toReplaceWithTransport);
+				ps.add_subscriber_to_channel(channel, *toReplaceWithTransport);
+			}
+		}
+
+		for (auto* pot_transport : potential_transport_set) {
+			SPDLOG_DEBUG("Potential Subscriber: {}  rtt: {} on channel {}", pot_transport->dst_addr.to_string(), pot_transport->get_rtt(), channel);
+		}
+
+		for (auto* transport : transport_set) {
+			SPDLOG_DEBUG("Subscriber: {}  rtt: {} on channel {}", transport->dst_addr.to_string(),  transport->get_rtt(), channel);
+		}
+	}
+
+	DefaultMulticastClient(DefaultMulticastClientOptions const& options = DefaultMulticastClientOptions())
+	: b(net::SocketAddress::from_string(options.discovery_addr)),
+	  ps(net::SocketAddress::from_string(options.pubsub_addr)),
+	  channels(options.channels) {
+		SPDLOG_INFO(
+			"Beacon: {}, Discovery: {}, PubSub: {}",
+			options.beacon_addr,
+			options.discovery_addr,
+			options.pubsub_addr
+		);
+
+		b.delegate = this;
+		ps.delegate = this;
+
+		b.start_discovery(net::SocketAddress::from_string(options.beacon_addr));
+	}
+
+	static int run_event_loop() {
+		return uv_run(uv_default_loop(), UV_RUN_DEFAULT);
+	}
+};
+
+} // namespace multicast
+} // namespace marlin
+
+#endif // MARLIN_MULTICAST_DEFAULTMULTICASTCLIENT_HPP
