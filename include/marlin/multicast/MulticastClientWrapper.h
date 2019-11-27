4fc4210f36abaec9de8d3a479a5f22bb5fd64219
amolfound <amol>
2019-11-27 13:57:01 +0530

    minimal working version

--- include/marlin/multicast/MulticastClientWrapper.h
+++ include/marlin/multicast/MulticastClientWrapper.h
@@ -1,32 +1,29 @@
+#include <stdint.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 struct MulticastClientDelegate {
-	// void (*did_recv_message)(
-	// 	char* message,
-	// 	uint64_t message_length,
-	// 	char* channel,
-	// 	uint64_t channel_length,
-	//  	uint64_t message_id
-	//  );
-
-	void (*did_recv_message)();
+	void (*did_recv_message)(
+		const char* message,
+		uint64_t message_length,
+		const char* channel,
+		uint64_t channel_length,
+	 	uint64_t message_id
+	 );
 };
 typedef struct MulticastClientDelegate MulticastClientDelegate_t;
 
 struct MulticastClientWrapper;
 typedef struct MulticastClientWrapper MulticastClientWrapper_t;
 
-MulticastClientWrapper_t* create_multicastclientwrapper();
+MulticastClientDelegate_t* create_multicastclientdelegate();
+MulticastClientWrapper_t* create_multicastclientwrapper(char* beacon_addr, char* discovery_addr, char* pubsub_addr);
 void setDelegate(MulticastClientWrapper_t* mc_w, MulticastClientDelegate_t* mc_d);
+
+int run_event_loop();
 
 #ifdef __cplusplus
 }
 #endif
-
-// void set_delegate(void *client, MulticastDelegate *del);
-
-// void set_delegate(void *client, MulticastDelegate *del) {
-// 	(DefaultMulticastClient<MulticastDelegateWrapper> *)client->del = new MulticastDelegateWrapper(del);
-// }
--- src/cwrappermulticasttest.c
+++ src/cwrappermulticasttest.c
@@ -1,25 +1,26 @@
+#include <stdio.h>
 #include <marlin/multicast/MulticastClientWrapper.h>
 
-// void  did_recv_message (
-// 	char* message,
-// 	uint64_t message_length,
-// 	char* channel,
-// 	uint64_t channel_length,
-// 	uint64_t message_id) {
+void  did_recv_message (
+	const char* message,
+	uint64_t message_length,
+	const char* channel,
+	uint64_t channel_length,
+	uint64_t message_id) {
 
-// }
-
-void did_recv_message() {
+	printf("C message:%s, channel:%s, message_id: %llu \n", message, channel, message_id);
 }
 
 int main() {
-	MulticastClientDelegate_t *mc_d;
+	MulticastClientDelegate_t *mc_d = create_multicastclientdelegate();
+
 	mc_d->did_recv_message = did_recv_message;
 
-	MulticastClientWrapper_t *m1 = create_multicastclientwrapper();
-	setDelegate(m1, mc_d);
-	// MulticastClientWrapper m2 = create_multicastclient(options);
+	MulticastClientWrapper_t *m2 = create_multicastclientwrapper("127.0.0.1:9002", "127.0.0.1:7002", "127.0.0.1:7000");
+	setDelegate(m2, mc_d);
 
-	// set delegate's void functions
-	// set
+	MulticastClientWrapper_t *m1 = create_multicastclientwrapper("127.0.0.1:9002", "127.0.0.1:8002", "127.0.0.1:8000");
+	setDelegate(m1, mc_d);
+
+	return run_event_loop();
 }
