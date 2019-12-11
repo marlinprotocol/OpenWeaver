1d04786492405853507d8ba78121bbf6883ccc39
Roshan Raghupathy <thisisroshansmail@gmail.com>
2019-12-11 10:38:54 +0530

    Add CTR related methods, separate STF buffer per stream

--- include/marlin/lpf/LpfTransport.hpp
+++ include/marlin/lpf/LpfTransport.hpp
@@ -1,12 +1,16 @@
 #ifndef MARLIN_LPF_LPFTRANSPORT_HPP
 #define MARLIN_LPF_LPFTRANSPORT_HPP
 
+#include <list>
+#include <unordered_set>
 #include <spdlog/spdlog.h>
 
 #include <marlin/net/SocketAddress.hpp>
 #include <marlin/net/Buffer.hpp>
 #include <marlin/net/core/TransportManager.hpp>
 
+#include <marlin/lpf/CutThroughBuffer.hpp>
+#include <marlin/lpf/StoreThenForwardBuffer.hpp>
 
 namespace marlin {
 namespace lpf {
@@ -20,21 +24,21 @@ private:
 	BaseTransport &transport;
 	net::TransportManager<Self> &transport_manager;
 
-	uint64_t length = 0;
-	char *buf = nullptr;
-	uint64_t size = 0;
-
+	std::unordered_map<uint16_t, StoreThenForwardBuffer> stf_buffers;
 public:
+	void did_recv_stf_message(uint16_t id, net::Buffer &&message);
+
 	// Delegate
 	void did_dial(BaseTransport &transport);
-	void did_recv_bytes(BaseTransport &transport, net::Buffer &&bytes);
+	void did_recv_bytes(BaseTransport &transport, net::Buffer &&bytes, uint16_t stream_id = 0);
 	void did_send_bytes(BaseTransport &transport, net::Buffer &&bytes);
 	void did_close(BaseTransport& transport);
+	void did_recv_flush_stream(BaseTransport &transport, uint16_t id, uint64_t offset, uint64_t old_offset);
 
 	net::SocketAddress src_addr;
 	net::SocketAddress dst_addr;
 
-	DelegateType *delegate;
+	DelegateType *delegate = nullptr;
 
 	LpfTransport(
 		net::SocketAddress const &src_addr,
@@ -49,10 +53,36 @@ public:
 
 	bool is_active();
 	double get_rtt();
+
+	bool should_cut_through = false;
+
+	int cut_through_send(net::Buffer &&message);
+private:
+	std::unordered_map<uint16_t, CutThroughBuffer> cut_through_buffers;
+	std::list<uint16_t> cut_through_reserve_ids = {10, 11, 12, 13, 14, 15, 16, 17, 18, 19};
+public:
+	std::unordered_set<uint16_t> cut_through_used_ids;
+	uint16_t cut_through_send_start(uint64_t length);
+	int cut_through_send_bytes(uint16_t id, net::Buffer &&bytes);
+	void cut_through_send_end(uint16_t id);
+	void cut_through_send_reset(uint16_t id);
+
+	void cut_through_recv_start(uint16_t id, uint64_t length);
+	void cut_through_recv_bytes(uint16_t id, net::Buffer &&bytes);
+	void cut_through_recv_end(uint16_t id);
+	void cut_through_recv_reset(uint16_t id);
 };
 
 
 // Impl
+
+template<typename DelegateType, template<typename> class StreamTransportType, int prefix_length>
+void LpfTransport<DelegateType, StreamTransportType, prefix_length>::did_recv_stf_message(
+	uint16_t,
+	net::Buffer &&message
+) {
+	delegate->did_recv_message(*this, std::move(message));
+}
 
 template<typename DelegateType, template<typename> class StreamTransportType, int prefix_length>
 void LpfTransport<DelegateType, StreamTransportType, prefix_length>::did_dial(
@@ -64,49 +94,34 @@ void LpfTransport<DelegateType, StreamTransportType, prefix_length>::did_dial(
 template<typename DelegateType, template<typename> class StreamTransportType, int prefix_length>
 void LpfTransport<DelegateType, StreamTransportType, prefix_length>::did_recv_bytes(
 	BaseTransport &,
-	net::Buffer &&bytes
+	net::Buffer &&bytes,
+	uint16_t stream_id
 ) {
-	if(buf == nullptr) { // Read length
-		if(bytes.size() + size < 8) { // Partial length
-			for(size_t i = 0; i < bytes.size(); i++) {
-				length = (length << 8) | bytes.data()[i];
-			}
-			size += bytes.size();
-		} else { // Full length
-			for(size_t i = 0; i < 8 - size; i++) {
-				length = (length << 8) | bytes.data()[i];
-			}
-			bytes.cover(8 - size);
+	if(should_cut_through && stream_id >= 10 && stream_id < 20) {
+		auto &rbuf = cut_through_buffers[stream_id];
 
-			if(length > 5000000) { // Abort on big message, DoS prevention
-				close();
-			}
-
-			// Prepare to process message
-			buf = new char[length];
-			size = 0;
-
-			// Process remaining bytes
-			did_recv_bytes(transport, std::move(bytes));
+		if(rbuf.id == 0) { // New buf
+			rbuf.id = stream_id;
 		}
-	} else { // Read message
-		if(bytes.size() + size < length) { // Partial message
-			std::memcpy(buf + size, bytes.data(), bytes.size());
-			size += bytes.size();
-		} else { // Full message
-			std::memcpy(buf + size, bytes.data(), length - size);
-			bytes.cover(length - size);
 
-			delegate->did_recv_message(*this, net::Buffer(buf, length));
+		int res = rbuf.did_recv_bytes(*this, std::move(bytes));
 
-			// Prepare to process length
-			buf = nullptr;
-			size = 0;
-			length = 0;
-
-			// Process remaining bytes
-			did_recv_bytes(transport, std::move(bytes));
+		if(res < 0) {
+			close();
 		}
+
+		return;
+	}
+
+	auto &stfbuf = stf_buffers[stream_id];
+	if(stfbuf.id == 0) { // New buf
+		stfbuf.id = stream_id;
+	}
+
+	int res = stfbuf.did_recv_bytes(*this, std::move(bytes));
+
+	if(res < 0) {
+		close();
 	}
 }
 
@@ -123,9 +138,15 @@ template<typename DelegateType, template<typename> class StreamTransportType, in
 void LpfTransport<DelegateType, StreamTransportType, prefix_length>::did_close(
 	BaseTransport&
 ) {
-	delete buf;
 	delegate->did_close(*this);
+	transport_manager.erase(dst_addr);
 }
+
+template<typename DelegateType, template<typename> class StreamTransportType, int prefix_length>
+void LpfTransport<DelegateType, StreamTransportType, prefix_length>::did_recv_flush_stream(BaseTransport &, uint16_t id, uint64_t, uint64_t) {
+	cut_through_recv_reset(id);
+}
+
 
 template<typename DelegateType, template<typename> class StreamTransportType, int prefix_length>
 LpfTransport<DelegateType, StreamTransportType, prefix_length>::LpfTransport(
@@ -133,7 +154,7 @@ LpfTransport<DelegateType, StreamTransportType, prefix_length>::LpfTransport(
 	net::SocketAddress const &dst_addr,
 	BaseTransport &transport,
 	net::TransportManager<LpfTransport<DelegateType, StreamTransportType, prefix_length>> &transport_manager
-) : transport(transport), transport_manager(transport_manager), src_addr(src_addr), dst_addr(dst_addr) {}
+) : transport(transport), transport_manager(transport_manager), src_addr(src_addr), dst_addr(dst_addr), delegate(nullptr) {}
 
 template<typename DelegateType, template<typename> class StreamTransportType, int prefix_length>
 void LpfTransport<DelegateType, StreamTransportType, prefix_length>::setup(
@@ -157,10 +178,28 @@ int LpfTransport<DelegateType, StreamTransportType, prefix_length>::send(
 }
 
 template<typename DelegateType, template<typename> class StreamTransportType, int prefix_length>
+int LpfTransport<DelegateType, StreamTransportType, prefix_length>::cut_through_send(
+	net::Buffer &&message
+) {
+	auto id = cut_through_send_start(message.size());
+	if(id == 0) {
+		return send(std::move(message));
+	}
+
+	auto res = cut_through_send_bytes(id, std::move(message));
+
+	if(res < 0) {
+		return res;
+	}
+
+	cut_through_send_end(id);
+
+	return 0;
+}
+
+template<typename DelegateType, template<typename> class StreamTransportType, int prefix_length>
 void LpfTransport<DelegateType, StreamTransportType, prefix_length>::close() {
 	transport.close();
-	delegate->did_close(*this);
-	transport_manager.erase(dst_addr);
 }
 
 template<typename DelegateType, template<typename> class StreamTransportType, int prefix_length>
@@ -171,6 +210,57 @@ bool LpfTransport<DelegateType, StreamTransportType, prefix_length>::is_active()
 template<typename DelegateType, template<typename> class StreamTransportType, int prefix_length>
 double LpfTransport<DelegateType, StreamTransportType, prefix_length>::get_rtt() {
 	return transport.get_rtt();
+}
+
+template<typename DelegateType, template<typename> class StreamTransportType, int prefix_length>
+uint16_t LpfTransport<DelegateType, StreamTransportType, prefix_length>::cut_through_send_start(uint64_t length) {
+	auto id = cut_through_reserve_ids.front();
+	cut_through_reserve_ids.pop_front();
+	cut_through_used_ids.insert(id);
+
+	net::Buffer m(new char[8], 8);
+	m.write_uint64_be(0, length);
+	auto res = transport.send(std::move(m), id);
+
+	if(res < 0) return 0;
+
+	return id;
+}
+
+template<typename DelegateType, template<typename> class StreamTransportType, int prefix_length>
+int LpfTransport<DelegateType, StreamTransportType, prefix_length>::cut_through_send_bytes(uint16_t id, net::Buffer &&bytes) {
+	return transport.send(std::move(bytes), id);
+}
+
+template<typename DelegateType, template<typename> class StreamTransportType, int prefix_length>
+void LpfTransport<DelegateType, StreamTransportType, prefix_length>::cut_through_send_end(uint16_t id) {
+	cut_through_used_ids.erase(id);
+	cut_through_reserve_ids.push_back(id);
+}
+
+template<typename DelegateType, template<typename> class StreamTransportType, int prefix_length>
+void LpfTransport<DelegateType, StreamTransportType, prefix_length>::cut_through_send_reset(uint16_t id) {
+	transport.flush_stream(id);
+}
+
+template<typename DelegateType, template<typename> class StreamTransportType, int prefix_length>
+void LpfTransport<DelegateType, StreamTransportType, prefix_length>::cut_through_recv_start(uint16_t id, uint64_t length) {
+	delegate->cut_through_recv_start(*this, id, length);
+}
+
+template<typename DelegateType, template<typename> class StreamTransportType, int prefix_length>
+void LpfTransport<DelegateType, StreamTransportType, prefix_length>::cut_through_recv_bytes(uint16_t id, net::Buffer &&bytes) {
+	delegate->cut_through_recv_bytes(*this, id, std::move(bytes));
+}
+
+template<typename DelegateType, template<typename> class StreamTransportType, int prefix_length>
+void LpfTransport<DelegateType, StreamTransportType, prefix_length>::cut_through_recv_end(uint16_t id) {
+	delegate->cut_through_recv_end(*this, id);
+}
+
+template<typename DelegateType, template<typename> class StreamTransportType, int prefix_length>
+void LpfTransport<DelegateType, StreamTransportType, prefix_length>::cut_through_recv_reset(uint16_t id) {
+	delegate->cut_through_recv_reset(*this, id);
 }
 
 } // namespace lpf
