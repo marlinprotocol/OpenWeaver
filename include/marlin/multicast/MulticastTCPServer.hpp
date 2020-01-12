9ee138d0a7fb0d311f1e78db15c8fa52246114cb
Amol Agrawal <amolfound@gmail.com>
2020-01-12 12:29:20 +0530

    added support for multicastTcpServer

---
+++ include/marlin/multicast/MulticastTCPServer.hpp
@@ -0,0 +1,17 @@
+#ifndef MARLIN_MULTICAST_MULTICASTTCPSERVER_H
+#define MARLIN_MULTICAST_MULTICASTTCPSERVER_H
+
+#include <marlin/lpf/LPFTransport>
+
+namespace marlin {
+namespace multicast {
+
+template<typename Delegate>
+class MulticastTCPServer
+
+} // namespace multicast
+} // namespace marlin
+
+#endif // MARLIN_MULTICAST_MULTICASTTCPSERVER_H
+
+<LPFTransport>
