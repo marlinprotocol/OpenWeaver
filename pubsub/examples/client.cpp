34dfcde5ef458997eb68958888a1b990308ec70b
Roshan Raghupathy <thisisroshansmail@gmail.com>
2020-01-25 15:01:20 +0530

    pubsub: Fix build process

--- pubsub/examples/client.cpp
+++ pubsub/examples/client.cpp
@@ -3,8 +3,8 @@
 #include <uv.h>
 #include <cstring>
 #include <algorithm>
-#include <marlin/pubsub/PubSubServer.hpp>
-#include <marlin/pubsub/PubSubClient.hpp>
+#include <marlin/pubsub/PubSubNode.hpp>
+#include <sodium.h>
 
 
 using namespace marlin::net;
@@ -13,71 +13,63 @@ using namespace marlin::pubsub;
 using namespace std;
 
 
-class PubSubServerDelegate {
-public:
-	std::vector<std::string> channels = {"some_channel", "other_channel"};
+class PubSubNodeDelegate {
+private:
+	using PubSubNodeType = PubSubNode<PubSubNodeDelegate, true, true, true>;
 
-	void did_unsubscribe(PubSubServer<PubSubServerDelegate> &, std::string channel) {
+public:
+	std::vector<std::string> channels = {"some_chan", "other_chan"};
+
+	void did_unsubscribe(PubSubNodeType &, std::string channel) {
 		SPDLOG_INFO("Did unsubscribe: {}", channel);
 	}
 
-	void did_subscribe(PubSubServer<PubSubServerDelegate> &, std::string channel) {
+	void did_subscribe(PubSubNodeType &ps, std::string channel) {
+		ps.send_message_on_channel(channel, "hey", 3);
 		SPDLOG_INFO("Did subscribe: {}", channel);
 	}
 
-	void did_recv_message(PubSubServer<PubSubServerDelegate> &, Buffer &&message, std::string &channel, uint64_t message_id) {
-		SPDLOG_INFO("Received message {} on channel {}: {}", message_id, channel, spdlog::to_hex(message.data(), message.data() + message.size()));
-	}
-
-	void manage_subscribers(
-		std::string,
-		typename PubSubServer<PubSubServerDelegate>::TransportSet&,
-		typename PubSubServer<PubSubServerDelegate>::TransportSet&
+	void did_recv_message(
+		PubSubNodeType &,
+		Buffer &&message,
+		Buffer &&,
+		std::string &channel,
+		uint64_t message_id
 	) {
-
-	}
-};
-
-class PubSubClientDelegate {
-public:
-	std::vector<std::string> channels = {"some_channel", "other_channel"};
-
-	void did_unsubscribe(PubSubClient<PubSubClientDelegate> &, std::string channel) {
-		SPDLOG_INFO("Did unsubscribe: {}", channel);
-	}
-
-	void did_subscribe(PubSubClient<PubSubClientDelegate> &, std::string channel) {
-		SPDLOG_INFO("Did subscribe: {}", channel);
-	}
-
-	void did_recv_message(PubSubClient<PubSubClientDelegate> &, Buffer &&message, std::string &channel, uint64_t message_id) {
 		SPDLOG_INFO("Received message {} on channel {}: {}", message_id, channel, spdlog::to_hex(message.data(), message.data() + message.size()));
 	}
 
-	void manage_subscribers(
-		std::string,
-		typename PubSubClient<PubSubClientDelegate>::TransportSet&,
-		typename PubSubClient<PubSubClientDelegate>::TransportSet&
+	void manage_subscriptions(
+		size_t,
+		typename PubSubNodeType::TransportSet&,
+		typename PubSubNodeType::TransportSet&
 	) {
 
 	}
 };
 
 int main() {
-	PubSubClientDelegate b_del;
-	PubSubServerDelegate b2_del;
+	uint8_t static_sk[crypto_box_SECRETKEYBYTES];
+	uint8_t static_pk[crypto_box_PUBLICKEYBYTES];
+
+	crypto_box_keypair(static_pk, static_sk);
+
+	PubSubNodeDelegate b_del;
+
+	size_t max_sol_conn = 10;
+	size_t max_unsol_conn = 1;
 
 	auto addr = SocketAddress::from_string("127.0.0.1:8000");
-	auto b = new PubSubClient<PubSubClientDelegate>(addr);
+	auto b = new PubSubNode<PubSubNodeDelegate, true, true, true>(addr, max_sol_conn, max_unsol_conn, static_sk);
 	b->delegate = &b_del;
 
 	auto addr2 = SocketAddress::from_string("127.0.0.1:8001");
-	auto b2 = new PubSubServer<PubSubServerDelegate>(addr2);
-	b2->delegate = &b2_del;
+	auto b2 = new PubSubNode<PubSubNodeDelegate, true, true, true>(addr2, max_sol_conn, max_unsol_conn, static_sk);
+	b2->delegate = &b_del;
 
 	SPDLOG_INFO("Start");
 
-	b->dial(addr2);
+	b->dial(addr2, static_pk);
 
 	return uv_run(uv_default_loop(), UV_RUN_DEFAULT);
 }
