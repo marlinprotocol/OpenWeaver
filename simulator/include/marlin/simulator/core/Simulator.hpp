5b20645e85a1314b8ea7dc5cbf7a2d22ab5634ae
Roshan Raghupathy <thisisroshansmail@gmail.com>
2020-02-01 18:40:00 +0530

    Add basic simulator project

---
+++ simulator/include/marlin/simulator/core/Simulator.hpp
@@ -0,0 +1,15 @@
+#ifndef MARLIN_SIMULATOR_CORE_SIMULATOR_HPP
+#define MARLIN_SIMULATOR_CORE_SIMULATOR_HPP
+
+namespace marlin {
+namespace simulator {
+
+class Simulator {
+public:
+	Simulator();
+};
+
+} // namespace simulator
+} // namespace marlin
+
+#endif // MARLIN_SIMULATOR_CORE_SIMULATOR_HPP
---
+++ simulator/src/core/Simulator.cpp
@@ -0,0 +1,10 @@
+#include "marlin/simulator/core/Simulator.hpp"
+
+
+namespace marlin {
+namespace simulator {
+
+Simulator::Simulator() {}
+
+} // namespace simulator
+} // namespace marlin
