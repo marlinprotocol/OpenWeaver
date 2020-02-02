f3b1f886e07e64b6b50761511570892d174a0a23
Roshan Raghupathy <thisisroshansmail@gmail.com>
2020-02-03 00:46:57 +0530

    simulator: Move template code from cpp to hpp

--- simulator/include/marlin/simulator/core/Event.hpp
+++ simulator/include/marlin/simulator/core/Event.hpp
@@ -28,6 +28,18 @@ public:
 	virtual void run(EventManager &manager) = 0;
 };
 
+
+// Impl
+
+template<typename EventManager>
+uint64_t Event<EventManager>::id_seq = 0;
+
+template<typename EventManager>
+Event<EventManager>::Event(uint64_t tick) {
+	id = id_seq++;
+	this->tick = tick;
+}
+
 } // namespace simulator
 } // namespace marlin
 
