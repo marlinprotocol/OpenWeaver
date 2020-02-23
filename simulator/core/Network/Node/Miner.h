ddc1797ca085c774dab072c899053e5c3b0966bc
Siddhartha Dutta <siddhartha_dutta@live.com>
2020-02-23 22:48:43 +0530

    changed unique to shared ptr, added func getHashPower

--- simulator/core/Network/Node/Miner.h
+++ simulator/core/Network/Node/Miner.h
@@ -1,7 +1,6 @@
 #ifndef MINER_H_
 #define MINER_H_
 
-#include <random>
 #include <set>
 
 #include "./Node.h"
@@ -10,18 +9,15 @@
 
 class Miner : public Node {
 private:
-	std::mt19937_64 rng;
 	int miningEventId; // default: -1 = not mining
 	long long hashpower;
 	std::set<int> receivedBlocks;
-	std::exponential_distribution<double> exp;
 
 public:
 	Miner(int _nodeId, bool _isAlive, int _region, 
-		  std::unique_ptr<BlockchainManagementModel> _blockchainManagementModel,
+		  std::shared_ptr<BlockchainManagementModel> _blockchainManagementModel,
 		  std::shared_ptr<BlockCache> _blockCache);
-	uint64_t getRandomNumFromExponentialDistribtuion();
-	uint64_t getTicksUntilNextBlock();
+	long long getHashPower();
 	void onNewBlockIdMessage(std::shared_ptr<NewBlockIdMessage> _message, EventManager* _eventManager);
 	void onNewBlockMinedMessage(std::shared_ptr<NewBlockMinedMessage> _message, EventManager* _eventManager, uint64_t _currentTick);
 };
