b52d33d93093346315489e7a70c4d324ae4687c3
Siddhartha Dutta <siddhartha_dutta@live.com>
2020-02-23 22:50:01 +0530

    renamed files, added block production scheduling funcs

--- simulator/models/BlockchainManagementModels/BlockchainManagementBitcoinModel.cpp
+++
@@ -1,5 +0,0 @@
-#include "./BlockchainManagementBitcoinModel.h"
-
-void BlockchainManagementBitcoinModel::OnOutOfRangeNewBlockArrival() {
-
-}
--- simulator/models/BlockchainManagementModels/BlockchainManagementModel.h
+++ simulator/models/BlockchainManagementModels/BlockchainManagementModel.h
@@ -1,12 +1,14 @@
 #ifndef BLOCKCHAINMGMTMODEL_H_
 #define BLOCKCHAINMGMTMODEL_H_
 
+#include "../../core/Blockchain/Block/Block.h"
 #include "../../helpers/Logger/easylogging.h"
 
 class BlockchainManagementModel {
 public:
 	virtual ~BlockchainManagementModel() {}
 	virtual void OnOutOfRangeNewBlockArrival() = 0;
+	virtual std::shared_ptr<Block> createGenesisBlock() = 0;
 };
 
 #endif /*BLOCKCHAINMGMTMODEL_H_*/
