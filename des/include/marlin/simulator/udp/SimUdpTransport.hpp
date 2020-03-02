b8dc3441c27f43efbc9c4fa4bb3d7877bbdeecb7
Roshan Raghupathy <thisisroshansmail@gmail.com>
2020-03-02 17:16:53 +0530

    Start writing a transport to simulate UDP

---
+++ des/include/marlin/simulator/udp/SimUdpTransport.hpp
@@ -0,0 +1,22 @@
+#ifndef MARLIN_SIMULATOR_UDP_SIMUDPTRANSPORT_HPP
+#define MARLIN_SIMULATOR_UDP_SIMUDPTRANSPORT_HPP
+
+#include "marlin/simulator/network/DataOnLinkEvent.hpp"
+
+#include <marlin/net/Buffer.hpp>
+#include <marlin/net/SocketAddress.hpp>
+#include <marlin/net/core/TransportManager.hpp>
+
+
+namespace marlin {
+namespace simulator {
+
+template<typename DelegateType>
+class SimUdpTransport {
+};
+
+
+} // namespace simulator
+} // namespace marlin
+
+#endif // MARLIN_SIMULATOR_UDP_SIMUDPTRANSPORT_HPP
