e23a99bfc968f1b8b912849a18a452a60154d9a5
Roshan Raghupathy <thisisroshansmail@gmail.com>
2020-03-02 19:49:29 +0530

    Add preliminary implementations, need to test

--- des/include/marlin/simulator/transport/SimulatedTransport.hpp
+++ des/include/marlin/simulator/transport/SimulatedTransport.hpp
@@ -62,6 +62,77 @@ public:
 };
 
 
+// Impl
+
+template<
+	typename EventManager,
+	typename MessageType,
+	typename DelegateType
+>
+SimulatedTransport<
+	EventManager,
+	MessageType,
+	DelegateType
+>::SimulatedTransport(
+	SocketAddress const& src_addr,
+	SocketAddress const& dst_addr,
+	NetworkLinkType& link,
+	TransportManager<Self>& transport_manager,
+	EventManager& manager
+) : link(link), transport_manager(transport_manager),
+	src_addr(src_addr), dst_addr(dst_addr), manager(manager) {}
+
+
+template<
+	typename EventManager,
+	typename MessageType,
+	typename DelegateType
+>
+void SimulatedTransport<
+	EventManager,
+	MessageType,
+	DelegateType
+>::setup(DelegateType* delegate) {
+	this->delegate = delegate;
+}
+
+template<
+	typename EventManager,
+	typename MessageType,
+	typename DelegateType
+>
+int SimulatedTransport<
+	EventManager,
+	MessageType,
+	DelegateType
+>::send(Buffer&& buf) {
+	if(&link.src == this) {
+		link.send_to_dst(manager, manager.current_tick(), std::move(message));
+	} else {
+		link.send_to_src(manager, manager.current_tick(), std::move(message));
+	}
+
+	return 0;
+}
+
+template<
+	typename EventManager,
+	typename MessageType,
+	typename DelegateType
+>
+void SimulatedTransport<
+	EventManager,
+	MessageType,
+	DelegateType
+>::did_recv(
+	typename NetworkLinkType::SrcEventType&,
+	typename NetworkLinkType::EventManager&,
+	typename NetworkLinkType::MessageType&& message,
+	NetworkLinkType&
+) {
+	delegate->did_recv_packet(std::move(message));
+}
+
 } // namespace simulator
 } // namespace marlin
 
