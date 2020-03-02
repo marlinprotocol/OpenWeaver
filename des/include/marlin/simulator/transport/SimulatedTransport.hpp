ca2ee8154120fb4e260be07c04e36749d7105906
Roshan Raghupathy <thisisroshansmail@gmail.com>
2020-03-02 17:33:34 +0530

    Copy public interface from UDP/TCP transports

--- des/include/marlin/simulator/transport/SimulatedTransport.hpp
+++ des/include/marlin/simulator/transport/SimulatedTransport.hpp
@@ -1,7 +1,7 @@
 #ifndef MARLIN_SIMULATOR_UDP_SIMULATEDTRANSPORT_HPP
 #define MARLIN_SIMULATOR_UDP_SIMULATEDTRANSPORT_HPP
 
-#include "marlin/simulator/network/DataOnLinkEvent.hpp"
+#include "marlin/simulator/network/NetworkLink.hpp"
 
 #include <marlin/net/Buffer.hpp>
 #include <marlin/net/SocketAddress.hpp>
@@ -13,6 +13,27 @@ namespace simulator {
 
 template<typename DelegateType>
 class SimulatedTransport {
+private:
+	NetworkLink& link;
+public:
+	using SelfType = SimulatedTransport<DelegateType>;
+
+	SocketAddress src_addr;
+	SocketAddress dst_addr;
+
+	DelegateType* delegate = nullptr;
+
+	SimulatedTransport(
+		SocketAddress const &src_addr,
+		SocketAddress const &dst_addr,
+		NetworkLink& link,
+		TransportManager<SelfType> &transport_manager
+	);
+
+	void setup(DelegateType *delegate);
+	void did_recv_packet(Buffer &&packet);
+	int send(Buffer &&packet);
+	void close();
 };
 
 
