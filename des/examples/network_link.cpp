639d823f8fe0893f695942710a11477cfc10a31d
Roshan Raghupathy <thisisroshansmail@gmail.com>
2020-03-02 10:15:23 +0530

    Modify example to match new API

--- des/examples/network_link.cpp
+++ des/examples/network_link.cpp
@@ -6,31 +6,68 @@
 using namespace marlin::simulator;
 using namespace std;
 
+struct PingPongNode {
+	template<typename NetworkLink>
+	void did_recv(
+		typename NetworkLink::SrcEventType& event,
+		typename NetworkLink::EventManager& manager,
+		typename NetworkLink::MessageType&& message,
+		NetworkLink& link
+	) {
+		if(&link.src == this) {
+			cout<<"Src did recv: "<<event.get_tick()<<endl;
 
-class PingPongNetworkLink final : public NetworkLink<EventQueue, vector<uint8_t>> {
-public:
-	PingPongNetworkLink() : NetworkLink<EventQueue, vector<uint8_t>>(
-		bind(&PingPongNetworkLink::did_recv, this, placeholders::_1, placeholders::_2, placeholders::_3),
-		bind(&PingPongNetworkLink::did_recv, this, placeholders::_1, placeholders::_2, placeholders::_3)
-	) {}
+			if(event.get_tick() >= 10) return;
+
+			link.send_to_dst(manager, event.get_tick(), std::move(message));
+		} else {
+			cout<<"Dst did recv: "<<event.get_tick()<<endl;
+
+			if(event.get_tick() >= 10) return;
+
+			link.send_to_src(manager, event.get_tick(), std::move(message));
+		}
+	}
+};
+
+template<
+	typename EventManager,
+	typename MessageType,
+	typename SrcType,
+	typename DstType
+>
+struct UniformDelayLink : public NetworkLink<
+	EventManager,
+	MessageType,
+	SrcType,
+	DstType
+> {
+	using BaseLink = NetworkLink<
+		EventManager,
+		MessageType,
+		SrcType,
+		DstType
+	>;
+
+	using BaseLink::NetworkLink;
 
 	uint64_t get_out_tick(uint64_t in_tick) override {
 		return in_tick + 2;
 	}
 
-	void did_recv(vector<uint8_t>&& message, uint64_t tick, EventQueue& manager) {
-		cout<<"Did recv: "<<tick<<endl;
-
-		if(tick >= 10) return;
-
-		send(std::move(message), true, tick, manager);
-	}
 };
+
 
 int main() {
 	Simulator simulator;
-	PingPongNetworkLink link;
-	link.send({0,1,2,3,4}, true, 1, simulator.queue);
+	PingPongNode n;
+	UniformDelayLink<
+		EventQueue,
+		std::vector<uint8_t>,
+		PingPongNode,
+		PingPongNode
+	> link(n, n);
+	link.send_to_src(simulator.queue, 1, {0,1,2,3,4});
 
 	cout<<"Simulation start"<<endl;
 
