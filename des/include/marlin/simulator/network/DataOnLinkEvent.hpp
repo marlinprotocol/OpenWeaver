03883ff8ad38541839068d829d104ced1ad694d0
Roshan Raghupathy <thisisroshansmail@gmail.com>
2020-03-02 10:14:42 +0530

    Better APIs instead of std::function

--- des/include/marlin/simulator/network/DataOnLinkEvent.hpp
+++ des/include/marlin/simulator/network/DataOnLinkEvent.hpp
@@ -9,21 +9,40 @@
 namespace marlin {
 namespace simulator {
 
-template<typename EventManager, typename MessageType>
+template<
+	typename EventManager,
+	typename MessageType,
+	typename TargetType,
+	typename ...Args
+>
 class DataOnLinkEvent : public Event<EventManager> {
 private:
 	MessageType message;
-	std::function<void(MessageType &&, uint64_t, EventManager&)> f_did_recv;
+	TargetType &target;
+	std::tuple<Args...> args;
+
+protected:
+	template<size_t ...I>
+	void run_impl(EventManager& manager, std::index_sequence<I...>) {
+		target.did_recv(
+			*this,
+			manager,
+			std::move(message),
+			std::get<I>(args)...
+		);
+	}
+
 public:
 	DataOnLinkEvent(
 		uint64_t tick,
 		MessageType &&message,
-		std::function<void(MessageType &&, uint64_t, EventManager&)> f_did_recv
-	) : Event<EventManager>(tick), message(std::move(message)), f_did_recv(f_did_recv) {}
+		TargetType &target,
+		Args&&... args
+	) : Event<EventManager>(tick), message(std::move(message)), target(target), args(std::forward<Args>(args)...) {}
 	virtual ~DataOnLinkEvent() {}
 
-	void run(EventManager& manager) override {
-		f_did_recv(std::move(message), this->tick, manager);
+	virtual void run(EventManager& manager) override {
+		run_impl(manager, std::index_sequence_for<Args...>{});
 	}
 };
 
