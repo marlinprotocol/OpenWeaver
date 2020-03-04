fc1d1b41d025d5694f24acb06e14525655250530
Roshan Raghupathy <thisisroshansmail@gmail.com>
2020-03-04 13:05:28 +0530

    Add preliminary implementation for Network methods

--- des/include/marlin/simulator/network/Network.hpp
+++ des/include/marlin/simulator/network/Network.hpp
@@ -24,6 +24,39 @@ public:
 	);
 };
 
+
+// Impl
+
+NetworkInterface<Network>& Network::get_or_create_interface(
+	net::SocketAddress addr
+) {
+	addr.set_port(0);
+
+	return interfaces.try_emplace(
+		addr,
+		*this,
+		addr
+	).first->second;
+}
+
+int Network::send(
+	net::SocketAddress const& src_addr,
+	net::SocketAddress& dst_addr,
+	net::Buffer&& packet
+) {
+	auto port = dst_addr.port();
+	dst_addr.set_port(0);
+
+	if(interfaces.find(dst_addr) == interfaces.end()) {
+		return -1;
+	}
+
+	auto interface = interfaces[dst_addr];
+	interface.did_recv(port, src_addr, std::move(packet));
+
+	return 0;
+}
+
 } // namespace simulator
 } // namespace marlin
 
