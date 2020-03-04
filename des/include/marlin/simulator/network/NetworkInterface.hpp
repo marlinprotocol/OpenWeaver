2ae9f3346af5afc468e97491809837592189a317
Roshan Raghupathy <thisisroshansmail@gmail.com>
2020-03-04 12:51:30 +0530

    Add tentative implementations

--- des/include/marlin/simulator/network/NetworkInterface.hpp
+++ des/include/marlin/simulator/network/NetworkInterface.hpp
@@ -36,6 +36,65 @@ public:
 	void did_recv(uint16_t port, net::SocketAddress const& addr, net::Buffer&& packet);
 };
 
+
+// Impl
+
+template<typename NetworkType>
+NetworkInterface<NetworkType>::NetworkInterface(
+	NetworkType& network,
+	net::SocketAddress const& addr
+) : network(network), addr(addr) {}
+
+template<typename NetworkType>
+int NetworkInterface<NetworkType>::bind(NetworkListener& listener, uint16_t port) {
+	if(listeners.find(port) != listeners.end()) {
+		return -1;
+	}
+
+	listeners[port] = &listener;
+
+	return 0;
+}
+
+template<typename NetworkType>
+void NetworkInterface<NetworkType>::close(uint16_t port) {
+	if(listeners.find(port) == listeners.end()) {
+		return;
+	}
+
+	auto* listener = listeners[port];
+	listeners.erase(port);
+	listener->did_close();
+}
+
+template<typename NetworkType>
+int NetworkInterface<NetworkType>::send(
+	uint16_t port,
+	SocketAddress const& addr,
+	net::Buffer&& packet
+) {
+	auto src_addr = this->addr;
+	src_addr.set_port(port);
+
+	network.send(src_addr, addr, std::move(packet));
+
+	return 0;
+}
+
+template<typename NetworkType>
+void NetworkInterface<NetworkType>::did_recv(
+	uint16_t port,
+	SocketAddress const& addr,
+	net::Buffer&& packet
+) {
+	if(listeners.find(port) == listeners.end()) {
+		throw;
+	}
+
+	auto* listener = listeners[port];
+	listener->did_recv(addr, std::move(packet));
+}
+
 } // namespace simulator
 } // namespace marlin
 
