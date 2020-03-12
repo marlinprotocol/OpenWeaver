9f9e755a6e0d1300666bfb59f5ab287df7f2cf1d
Roshan Raghupathy <thisisroshansmail@gmail.com>
2020-03-12 08:18:45 +0530

    Add new event class to send messages to network interfaces

---
+++ des/include/marlin/simulator/network/DataOnInterfaceEvent.hpp
@@ -0,0 +1,40 @@
+#ifndef MARLIN_SIMULATOR_NETWORK_DATAONINTERFACEEVENT_HPP
+#define MARLIN_SIMULATOR_NETWORK_DATAONINTERFACEEVENT_HPP
+
+#include "marlin/simulator/core/Event.hpp"
+
+#include <marlin/net/SocketAddress.hpp>
+
+
+namespace marlin {
+namespace simulator {
+
+template<
+	typename EventManager,
+	typename MessageType,
+	typename TargetType
+>
+class DataOnInterfaceEvent : public Event<EventManager> {
+private:
+	net::SocketAddress src;
+	MessageType message;
+	TargetType& target;
+
+public:
+	DataOnInterfaceEvent(
+		uint64_t tick,
+		net::SocketAddress const& src,
+		MessageType &&message,
+		TargetType &target
+	) : Event<EventManager>(tick), src(src), message(std::move(message)), target(target) {}
+	virtual ~DataOnInterfaceEvent() {}
+
+	virtual void run(EventManager& manager) override {
+		target.did_recv(src, std::move(message));
+	}
+};
+
+} // namespace simulator
+} // namespace marlin
+
+#endif // MARLIN_SIMULATOR_NETWORK_DATAONINTERFACEEVENT_HPP
