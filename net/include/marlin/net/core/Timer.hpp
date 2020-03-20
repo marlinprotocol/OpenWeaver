9a1f3cab2f680a8b090f37404db29501bd5b3d50
Roshan Raghupathy <thisisroshansmail@gmail.com>
2020-03-20 15:29:17 +0530

    Add the ability to store random data in Timer which is returned in the callback

--- net/include/marlin/net/core/Timer.hpp
+++ net/include/marlin/net/core/Timer.hpp
@@ -5,6 +5,7 @@
 #define MARLIN_NET_TIMER_HPP
 
 #include <uv.h>
+#include <type_traits>
 
 
 namespace marlin {
@@ -19,15 +20,20 @@ private:
 	using Self = Timer<DelegateType>;
 
 	uv_timer_t* timer;
+	void* data = nullptr;
 
 	static void timer_close_cb(uv_handle_t* handle) {
 		delete (uv_timer_t*)handle;
 	}
 
-	template<void (DelegateType::*callback)()>
+	template<void (DelegateType::*callback)(), typename DataType>
 	static void timer_cb(uv_timer_t* handle) {
 		auto& timer = *(Self*)handle->data;
-		(timer.delegate->*callback)();
+		if constexpr (std::is_null_pointer_v<DataType>) {
+			(timer.delegate->*callback)();
+		} else {
+			(timer.delegate->*callback)((DataType*)timer.data);
+		}
 	}
 public:
 	DelegateType* delegate;
@@ -38,9 +44,14 @@ public:
 		uv_timer_init(uv_default_loop(), timer);
 	}
 
-	template<void (DelegateType::*callback)()>
+	template<typename DataType>
+	void set_data(DataType* data) {
+		this->data = (void*)data;
+	}
+
+	template<void (DelegateType::*callback)(), typename DataType = std::nullptr_t>
 	void start(uint64_t timeout, uint64_t repeat) {
-		uv_timer_start(timer, timer_cb<callback>, timeout, repeat);
+		uv_timer_start(timer, timer_cb<callback, DataType>, timeout, repeat);
 	}
 
 	void stop() {
