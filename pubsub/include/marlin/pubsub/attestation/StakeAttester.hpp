f676ac27b29e94efa5dd3b8d077c3c7eaf2a87fa
Roshan Raghupathy <thisisroshansmail@gmail.com>
2020-03-29 21:55:38 +0530

    Add preliminary cache and overlap checking

--- pubsub/include/marlin/pubsub/attestation/StakeAttester.hpp
+++ pubsub/include/marlin/pubsub/attestation/StakeAttester.hpp
@@ -16,6 +16,17 @@ struct StakeAttester {
 	ABCInterface& abci;
 	uint64_t stake_offset = 0;
 
+	struct Attestation {
+		uint64_t message_id;
+		uint64_t timestamp;
+		uint64_t stake_offset;
+		uint16_t channel_id;
+		uint8_t message_hash[32];
+		uint8_t sig[65];
+	};
+	std::vector<Attestation> attestation_cache;
+	std::unordered_map<std::string, std::map<uint64_t, Attestation*>> stake_caches;
+
 	secp256k1_context* ctx_signer = nullptr;
 	secp256k1_context* ctx_verifier = nullptr;
 
@@ -50,7 +61,7 @@ struct StakeAttester {
 		net::Buffer& out,
 		uint64_t offset = 0
 	) {
-		if(prev_header.attestation_data != nullptr) {
+		if(prev_header.attestation_size != 0) {
 			out.write(offset, prev_header.attestation_data, prev_header.attestation_size);
 			return 1;
 		}
@@ -60,7 +71,7 @@ struct StakeAttester {
 		out.write_uint64_be(offset+8, stake_offset);
 		stake_offset += message_size;
 
-		// TODO: Reset stake_offset at epochs
+		// TODO: Reset stake_offset and attestation cache at epochs
 
 		uint8_t hash[32];
 		CryptoPP::Keccak_256 hasher;
@@ -73,6 +84,8 @@ struct StakeAttester {
 		hasher.Update((uint8_t*)out.data()+offset, 16);
 		hasher.Update((uint8_t*)&message_size, 8);  // FIXME: Fix endian
 		hasher.Update(hash, 32);
+
+		hasher.TruncatedFinal(hash, 32);
 
 		// Get key
 		auto* key = abci.get_key();
@@ -93,7 +106,7 @@ struct StakeAttester {
 
 		if(res == 0) {
 			// Sign failed
-			return false;
+			return -2;
 		}
 
 		// Output
@@ -105,7 +118,7 @@ struct StakeAttester {
 			&sig
 		);
 
-		out.data()[80] = (char)recid;
+		out.data()[offset+80] = (uint8_t)recid;
 
 		return 0;
 	}
@@ -118,33 +131,37 @@ struct StakeAttester {
 		uint64_t message_size,
 		HeaderType prev_header
 	) {
+		auto& attestation = attestation_cache.emplace_back();
+
 		// Extract data
-		net::Buffer attestation((char*)prev_header.attestation_data, prev_header.attestation_size);
-		auto timestamp = attestation.read_uint64_be(0);
-		auto stake_offset = attestation.read_uint64_be(8);
-		attestation.release();
+		net::Buffer buf((char*)prev_header.attestation_data, prev_header.attestation_size);
+		attestation.timestamp = buf.read_uint64_be(0);
+		attestation.stake_offset = buf.read_uint64_be(8);
+		buf.release();
 
 		uint64_t now = std::time(nullptr);
 		// Permit a maximum clock skew of 60 seconds
-		if(now > timestamp && now - timestamp > 60) {
+		if(now > attestation.timestamp && now - attestation.timestamp > 60) {
 			// Too old
 			return false;
-		} else if(now < timestamp && timestamp - now > 60) {
+		} else if(now < attestation.timestamp && attestation.timestamp - now > 60) {
 			// Too new
 			return false;
 		}
 
-		uint8_t hash[32];
 		CryptoPP::Keccak_256 hasher;
 		// Hash message
-		hasher.CalculateTruncatedDigest(hash, 32, (uint8_t*)message_data, message_size);
+		hasher.CalculateTruncatedDigest(attestation.message_hash, 32, (uint8_t*)message_data, message_size);
 
 		// Hash for signature
 		hasher.Update((uint8_t*)&message_id, 8);  // FIXME: Fix endian
 		hasher.Update((uint8_t*)&channel, 2);  // FIXME: Fix endian
 		hasher.Update((uint8_t*)prev_header.attestation_data, 16);
 		hasher.Update((uint8_t*)&message_size, 8);  // FIXME: Fix endian
-		hasher.Update(hash, 32);
+		hasher.Update(attestation.message_hash, 32);
+
+		uint8_t hash[32];
+		hasher.TruncatedFinal(hash, 32);
 
 		// Parse signature
 		secp256k1_ecdsa_recoverable_signature sig;
@@ -157,20 +174,47 @@ struct StakeAttester {
 
 		// Verify signature
 		secp256k1_pubkey pubkey;
-		auto res = secp256k1_ecdsa_recover(
-			ctx_verifier,
-			&pubkey,
-			&sig,
-			hash
+		{
+			auto res = secp256k1_ecdsa_recover(
+				ctx_verifier,
+				&pubkey,
+				&sig,
+				hash
+			);
+
+			if(res == 0) {
+				// Recovery failed
+				return false;
+			}
+		}
+		// SPDLOG_INFO("Signature verified");
+
+		auto& stake_cache = stake_caches[std::string(pubkey.data, pubkey.data + 64)];
+
+		auto [iter_begin, res_begin] = stake_cache.try_emplace(
+			stake_offset,
+			&attestation
 		);
 
-		if(res == 0) {
-			// Recovery failed
+		if(!res_begin) {
+			// TODO: Handle overlap
 			return false;
 		}
 
-		// TODO: Cache stake and check for duplicates
-		(void)stake_offset;
+		auto [iter_end, res_end] = stake_cache.try_emplace(
+			stake_offset + message_size - 1,
+			&attestation
+		);
+
+		if(!res_end) {
+			// TODO: Handle overlap
+			return false;
+		}
+
+		for(iter_begin++; iter_begin != iter_end; iter_begin++) {
+			// TODO: Handle overlap
+			return false;
+		}
 
 		return true;
 	}
