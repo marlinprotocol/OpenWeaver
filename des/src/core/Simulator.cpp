4b6e9bd4f3982422d7f6a81b9166fad0d80b428b
Roshan Raghupathy <thisisroshansmail@gmail.com>
2020-04-05 18:41:50 +0530

    Add remove_event functionality

--- des/src/core/Simulator.cpp
+++ des/src/core/Simulator.cpp
@@ -16,6 +16,10 @@ void Simulator::add_event(std::shared_ptr<Event<Simulator>> event) {
 	queue.add_event(event);
 }
 
+void Simulator::remove_event(std::shared_ptr<Event<Simulator>> event) {
+	queue.remove_event(event);
+}
+
 void Simulator::run() {
 	while(!queue.is_empty()) {
 		auto event = queue.get_next_event();
