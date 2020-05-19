d865a05fa90dc71acc2abc240898d0fc6985852a
Roshan Raghupathy <thisisroshansmail@gmail.com>
2020-05-19 19:42:12 +0530

    ACK message

--- stream/include/marlin/stream/Messages.hpp
+++ stream/include/marlin/stream/Messages.hpp
@@ -153,6 +153,151 @@ struct DATA {
 };
 
 template<typename BaseMessageType>
+struct ACK {
+	BaseMessageType base;
+
+	[[nodiscard]] bool validate() const {
+		if(base.payload_buffer().size() < 20 || base.payload_buffer().size() != 20 + size()*8) {
+			return false;
+		}
+
+		return true;
+	}
+
+	ACK(size_t num_ranges) : base(20 + 8*num_ranges) {
+		base.set_payload({0, 2});
+	}
+
+	ACK(core::Buffer&& buf) : base(std::move(buf)) {}
+
+	ACK& set_src_conn_id(uint32_t src_conn_id) & {
+		base.payload_buffer().write_uint32_be_unsafe(2, src_conn_id);
+
+		return *this;
+	}
+
+	ACK&& set_src_conn_id(uint32_t src_conn_id) && {
+		return std::move(set_src_conn_id(src_conn_id));
+	}
+
+	uint32_t src_conn_id() const {
+		return base.payload_buffer().read_uint32_be_unsafe(6);
+	}
+
+	ACK& set_dst_conn_id(uint32_t dst_conn_id) & {
+		base.payload_buffer().write_uint32_be_unsafe(6, dst_conn_id);
+
+		return *this;
+	}
+
+	ACK&& set_dst_conn_id(uint32_t dst_conn_id) && {
+		return std::move(set_dst_conn_id(dst_conn_id));
+	}
+
+	uint32_t dst_conn_id() const {
+		return base.payload_buffer().read_uint32_be_unsafe(2);
+	}
+
+	ACK& set_size(uint16_t size) & {
+		base.payload_buffer().write_uint16_be_unsafe(10, size);
+
+		return *this;
+	}
+
+	ACK&& set_size(uint16_t size) && {
+		return std::move(set_size(size));
+	}
+
+	uint16_t size() const {
+		return base.payload_buffer().read_uint16_be_unsafe(10);
+	}
+
+	ACK& set_packet_number(uint64_t packet_number) & {
+		base.payload_buffer().write_uint64_be_unsafe(12, packet_number);
+
+		return *this;
+	}
+
+	ACK&& set_packet_number(uint64_t packet_number) && {
+		return std::move(set_packet_number(packet_number));
+	}
+
+	uint64_t packet_number() const {
+		return base.payload_buffer().read_uint64_be_unsafe(12);
+	}
+
+	struct iterator {
+	private:
+		core::WeakBuffer buf;
+		size_t offset = 0;
+	public:
+		// For iterator_traits
+		using difference_type = int32_t;
+		using value_type = uint64_t;
+		using pointer = value_type const*;
+		using reference = value_type const&;
+		using iterator_category = std::input_iterator_tag;
+
+		iterator(core::WeakBuffer buf, size_t offset = 0) : buf(buf), offset(offset) {}
+
+		value_type operator*() const {
+			return buf.read_uint64_be_unsafe(offset);
+		}
+
+		iterator& operator++() {
+			offset += 8;
+
+			return *this;
+		}
+
+		bool operator==(iterator const& other) const {
+			return offset == other.offset;
+		}
+
+		bool operator!=(iterator const& other) const {
+			return !(*this == other);
+		}
+	};
+
+	iterator ranges_begin() const {
+		return iterator(base.payload_buffer(), 20);
+	}
+
+	iterator ranges_end() const {
+		return iterator(base.payload_buffer(), 20 + size()*8);
+	}
+
+	template<typename It>
+	ACK& set_ranges(It begin, It end) & {
+		size_t idx = 20;
+		while(begin != end && idx + 8 <= base.payload_buffer().size()) {
+			base.payload_buffer().write_uint64_be_unsafe(idx, *begin);
+			idx += 8;
+			++begin;
+		}
+		base.truncate_unsafe(base.payload_buffer().size() - idx);
+
+		return *this;
+	}
+
+	template<typename It>
+	ACK&& set_ranges(It begin, It end) && {
+		return std::move(set_ranges(begin, end));
+	}
+
+	core::Buffer finalize() {
+		return base.finalize();
+	}
+
+	core::Buffer release() {
+		auto buf = base.release();
+		buf.cover_unsafe(30);
+
+		return std::move(buf);
+	}
+};
+
+template<typename BaseMessageType>
 struct DIAL {
 	BaseMessageType base;
 
