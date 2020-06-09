d01a3c69661e6a88f922ae87a54eca4ecba2b663
Roshan Raghupathy <thisisroshansmail@gmail.com>
2020-06-09 22:18:43 +0530

    Add a few keypairs that can be used in tests

--- mtest/include/marlin/mtest/NetworkFixture.hpp
+++ mtest/include/marlin/mtest/NetworkFixture.hpp
@@ -26,13 +26,30 @@ struct NetworkFixture : public ::testing::Test {
 	NetworkInterfaceType& i4;
 	NetworkInterfaceType& i5;
 
+	uint8_t static_sk1[crypto_box_SECRETKEYBYTES];
+	uint8_t static_pk1[crypto_box_PUBLICKEYBYTES];
+	uint8_t static_sk2[crypto_box_SECRETKEYBYTES];
+	uint8_t static_pk2[crypto_box_PUBLICKEYBYTES];
+	uint8_t static_sk3[crypto_box_SECRETKEYBYTES];
+	uint8_t static_pk3[crypto_box_PUBLICKEYBYTES];
+	uint8_t static_sk4[crypto_box_SECRETKEYBYTES];
+	uint8_t static_pk4[crypto_box_PUBLICKEYBYTES];
+	uint8_t static_sk5[crypto_box_SECRETKEYBYTES];
+	uint8_t static_pk5[crypto_box_PUBLICKEYBYTES];
+
 	NetworkFixture() :
 		network(nc),
 		i1(network.get_or_create_interface(core::SocketAddress::from_string("192.168.0.1:0"))),
 		i2(network.get_or_create_interface(core::SocketAddress::from_string("192.168.0.2:0"))),
 		i3(network.get_or_create_interface(core::SocketAddress::from_string("192.168.0.3:0"))),
 		i4(network.get_or_create_interface(core::SocketAddress::from_string("192.168.0.4:0"))),
-		i5(network.get_or_create_interface(core::SocketAddress::from_string("192.168.0.5:0"))) {}
+		i5(network.get_or_create_interface(core::SocketAddress::from_string("192.168.0.5:0"))) {
+		crypto_box_keypair(static_pk1, static_sk1);
+		crypto_box_keypair(static_pk2, static_sk2);
+		crypto_box_keypair(static_pk3, static_sk3);
+		crypto_box_keypair(static_pk4, static_sk4);
+		crypto_box_keypair(static_pk5, static_sk5);
+	}
 
 	template<typename Delegate>
 	using TransportType = simulator::SimulatedTransport<
