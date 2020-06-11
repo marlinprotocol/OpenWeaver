01ae75d28850402c7849f6a6c6ee0d8dcb2cc451
Roshan Raghupathy <thisisroshansmail@gmail.com>
2020-06-11 19:58:45 +0530

    Document BaseMessage

--- core/include/marlin/core/messages/BaseMessage.hpp
+++ core/include/marlin/core/messages/BaseMessage.hpp
@@ -1,6 +1,3 @@
-/*! \file BaseMessage.hpp
-*/
-
 #ifndef MARLIN_CORE_BASEMESSAGE_HPP
 #define MARLIN_CORE_BASEMESSAGE_HPP
 
@@ -9,23 +6,40 @@
 namespace marlin {
 namespace core {
 
+/// @brief Base message type with a payload
+/// @headerfile BaseMessage.hpp <marlin/core/BaseMessage.hpp>
 struct BaseMessage {
+	/// Underlying buffer
 	Buffer buf;
 
+	/// Construct from size
 	BaseMessage(size_t size);
+	/// Construct from a buffer
 	BaseMessage(Buffer&& buf);
 
+	/// Get a WeakBuffer corresponding to the payload area
 	WeakBuffer payload_buffer() const&;
+	/// Get a Buffer corresponding to the payload area, consumes the BaseMessage object
 	Buffer payload_buffer() &&;
+	/// Get a uint8_t* corresponding to the payload area
 	uint8_t* payload() const;
 
+	/// @{
+	/// Sets the payload given a byte address and size
 	BaseMessage& set_payload(uint8_t const* in, size_t size) &;
 	BaseMessage&& set_payload(uint8_t const* in, size_t size) &&;
+	/// @}
+	/// @{
+	/// Sets the payload given a initializer list with bytes
 	BaseMessage& set_payload(std::initializer_list<uint8_t> il) &;
 	BaseMessage&& set_payload(std::initializer_list<uint8_t> il) &&;
+	/// @}
 
+	/// @{
+	/// Truncates the underlying buffer to the given size
 	BaseMessage& truncate_unsafe(size_t size) &;
 	BaseMessage&& truncate_unsafe(size_t size) &&;
+	/// @}
 };
 
 } // namespace core
