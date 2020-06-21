6b9a41adac5862a8c47732cde78efaaa1c959abf
Roshan Raghupathy <thisisroshansmail@gmail.com>
2020-06-21 15:31:22 +0530

    Add compression method

--- compression/include/marlin/compression/BlockCompressor.hpp
+++ compression/include/marlin/compression/BlockCompressor.hpp
@@ -6,6 +6,7 @@
 #include <cryptopp/blake2.h>
 
 #include <unordered_map>
+#include <numeric>
 
 
 namespace marlin {
@@ -28,6 +29,61 @@ public:
 		);
 
 		txn_seen[txn_id] = asyncio::EventLoop::now();
+	}
+
+	core::Buffer compress(
+		std::vector<core::WeakBuffer> const& misc_bufs,
+		std::vector<core::WeakBuffer> const& txn_bufs
+	) {
+		// Compute total size of misc bufs
+		size_t misc_size = std::transform_reduce(
+			misc_bufs.begin(),
+			misc_bufs.end(),
+			0,
+			std::plus<>(),
+			[](core::WeakBuffer const& buf) { return buf.size(); }
+		);
+		size_t total_size = misc_size + txn_bufs.size()*8 + 10000; // Add space for txn ids and extra space for any full size txns
+		core::Buffer final_buf(total_size);
+		size_t offset = 0;
+
+		// Copy misc bufs
+		final_buf.write_uint64_le_unsafe(offset, misc_size);
+		offset += 8;
+		for(auto iter = misc_bufs.begin(); iter != misc_bufs.end(); iter++) {
+			final_buf.write_uint64_le_unsafe(offset, iter->size());
+			offset += 8;
+			final_buf.write_unsafe(offset, iter->data(), iter->size());
+			offset += iter->size();
+		}
+
+		CryptoPP::BLAKE2b blake2b((uint)8);
+		for(auto iter = txn_bufs.begin(); iter != txn_bufs.end(); iter++) {
+			blake2b.Update(iter->data(), iter->size());
+			uint64_t txn_id;
+			blake2b.TruncatedFinal((uint8_t*)&txn_id, 8);
+			auto txn_iter = txns.find(txn_id);
+
+			if(txn_iter == txns.end()) {
+				// Txn not in cache, encode in full
+				if(offset + txn_bufs.size()*8 > total_size)
+					throw; // TODO: Handle insufficient buffer size
+
+				final_buf.write_uint8_unsafe(offset, 0x00);
+				final_buf.write_uint64_le_unsafe(offset+1, iter->size());
+				final_buf.write_unsafe(offset+9, iter->data(), iter->size());
+
+				offset += 9 + iter->size();
+			} else {
+				// Found txn in cache, copy id
+				final_buf.write_uint8_unsafe(offset, 0x01);
+				final_buf.write_uint64_le_unsafe(offset+1, txn_id);
+
+				offset += 9;
+			}
+		}
+
+		return final_buf;
 	}
 };
 
