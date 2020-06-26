6ce92efd585d1dc6baf34391d117677f65997bd3
Roshan Raghupathy <thisisroshansmail@gmail.com>
2020-06-27 00:35:46 +0530

    Remove simulator, moved to other repo

--- simulator/core/EventManagement/Event/EventTypes/EventType.h
+++
@@ -1,6 +0,0 @@
-#ifndef EVENTTYPE_H_
-#define EVENTTYPE_H_
-
-enum class EventType {MESSAGE_TO_NODE};
-
-#endif /*EVENTTYPE_H_*/
--- simulator/models/Networking/BitcoinRoutingTable.cpp
+++
@@ -1,5 +0,0 @@
-#include "./BitcoinRoutingTable.h"
-
-BitcoinRoutingTable::BitcoinRoutingTable() : RoutingTable() {
-
-}
