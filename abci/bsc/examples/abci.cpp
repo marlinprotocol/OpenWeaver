ad9e69686020470be814873028e7180ebb4e13bd
Roshan Raghupathy <thisisroshansmail@gmail.com>
2020-08-25 11:15:56 +0530

    Get datadir from cli

--- abci/bsc/examples/abci.cpp
+++ abci/bsc/examples/abci.cpp
@@ -2,6 +2,8 @@
 
 #include <marlin/asyncio/core/EventLoop.hpp>
 
+#include <structopt/app.hpp>
+
 using namespace marlin::bsc;
 using namespace marlin::asyncio;
 
@@ -10,7 +12,19 @@ struct Delegate {
 	void did_connect(AbciType&) {}
 };
 
-int main() {
-	Abci<Delegate> abci("~/.bsc");
+struct Options {
+	std::string datadir;
+};
+STRUCTOPT(Options, datadir);
+
+int main(int argc, char** argv) {
+	try {
+		auto options = structopt::app("abci").parse<Options>(argc, argv);
+
+		Abci<Delegate> abci(options.datadir);
+	} catch (structopt::exception& e) {
+		SPDLOG_ERROR("{}", e.what());
+		SPDLOG_ERROR("{}", e.help());
+	}
 	return EventLoop::run();
 }
