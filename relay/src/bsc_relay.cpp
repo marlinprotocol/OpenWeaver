725cec0bc1a91b05555b1f89c227ff3e46589a74
Roshan Raghupathy <thisisroshansmail@gmail.com>
2020-09-12 18:31:05 +0530

    Rewrite relay to take structopt options

--- relay/src/bsc_relay.cpp
+++ relay/src/bsc_relay.cpp
@@ -1,31 +1,50 @@
 #include "Relay.hpp"
 #include <marlin/bsc/Abci.hpp>
 
+#include <structopt/app.hpp>
+
 using namespace marlin::core;
 using namespace marlin::asyncio;
 using namespace marlin::beacon;
 using namespace marlin::pubsub;
 
-#define RELAY_PUBSUB_PROTOCOL_NUMBER 0x10000000
 
-int main(int , char **argv) {
-	std::string beacon_addr(argv[1]);
-	uint32_t pubsub_port = 5000;
-	uint32_t discovery_port = 5002;
-	SPDLOG_INFO(
-		"Starting relay on pubsub port: {}, discovery_port: {}, beacon server: {}",
-		pubsub_port,
-		discovery_port,
-		beacon_addr
-	);
+struct CliOptions {
+	std::string beacon_addr;
+	std::string datadir;
+	std::optional<uint16_t> pubsub_port;
+	std::optional<uint16_t> discovery_port;
+};
+STRUCTOPT(CliOptions, beacon_addr, datadir, pubsub_port, discovery_port);
 
-	Relay<true, true, true, marlin::bsc::Abci> relay(
-		RELAY_PUBSUB_PROTOCOL_NUMBER,
-		pubsub_port,
-		SocketAddress::from_string("0.0.0.0:5000"),
-		SocketAddress::from_string("0.0.0.0:5002"),
-		SocketAddress::from_string(beacon_addr)
-	);
+int main(int argc, char** argv) {
+	try {
+		auto options = structopt::app("bsc_relay").parse<CliOptions>(argc, argv);
+		auto beacon_addr = options.beacon_addr;
+		auto datadir = options.datadir;
+		auto pubsub_port = options.pubsub_port.value_or(5000);
+		auto discovery_port = options.discovery_port.value_or(5002);
 
-	return EventLoop::run();
+		SPDLOG_INFO(
+			"Starting relay on pubsub port: {}, discovery_port: {}, beacon server: {}",
+			pubsub_port,
+			discovery_port,
+			beacon_addr
+		);
+
+		Relay<true, true, true, marlin::bsc::Abci> relay(
+			RELAY_PUBSUB_PROTOCOL_NUMBER,
+			pubsub_port,
+			SocketAddress::from_string(std::string("0.0.0.0:").append(std::to_string(pubsub_port))),
+			SocketAddress::from_string(std::string("0.0.0.0:").append(std::to_string(discovery_port))),
+			SocketAddress::from_string(beacon_addr)
+		);
+
+		return EventLoop::run();
+	} catch (structopt::exception& e) {
+		SPDLOG_ERROR("{}", e.what());
+		SPDLOG_ERROR("{}", e.help());
+	}
+
+	return -1;
 }
