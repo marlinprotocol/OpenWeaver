9f4797d56d79f6365611a15102e7426c4c05cdc5
Roshan Raghupathy <thisisroshansmail@gmail.com>
2020-09-22 12:44:59 +0530

    Rewrite beacon server with structopt

--- beacon/examples/server.cpp
+++ beacon/examples/server.cpp
@@ -1,18 +1,39 @@
 #include <marlin/beacon/DiscoveryServer.hpp>
 #include <marlin/asyncio/core/EventLoop.hpp>
 #include <cstring>
+#include <structopt/app.hpp>
 
 using namespace marlin;
 
 class BeaconDelegate {};
 
-int main() {
-	auto baddr = core::SocketAddress::from_string("0.0.0.0:8002");
-	auto haddr = core::SocketAddress::from_string("0.0.0.0:8003");
-	BeaconDelegate del;
+struct CliOptions {
+	std::string discovery_addr;
+	std::string heartbeat_addr;
+};
+STRUCTOPT(CliOptions, discovery_addr, heartbeat_addr);
 
-	auto b = new beacon::DiscoveryServer<BeaconDelegate>(baddr, haddr);
-	b->delegate = &del;
+int main(int argc, char** argv) {
+	try {
+		auto options = structopt::app("beacon").parse<CliOptions>(argc, argv);
+		auto discovery_addr = core::SocketAddress::from_string(options.discovery_addr);
+		auto heartbeat_addr = core::SocketAddress::from_string(options.heartbeat_addr);
 
-	return asyncio::EventLoop::run();
+		SPDLOG_INFO(
+			"Starting beacon with discovery: {}, heartbeat: {}",
+			discovery_addr.to_string(),
+			heartbeat_addr.to_string()
+		);
+
+		BeaconDelegate del;
+		auto b = new beacon::DiscoveryServer<BeaconDelegate>(discovery_addr, heartbeat_addr);
+		b->delegate = &del;
+
+		return asyncio::EventLoop::run();
+	} catch (structopt::exception& e) {
+		SPDLOG_ERROR("{}", e.what());
+		SPDLOG_ERROR("{}", e.help());
+	}
+
+	return -1;
 }
