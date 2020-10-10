ab82275b42c888b07a9a2b8b49e8fb2f8ae487fc
Roshan Raghupathy <thisisroshansmail@gmail.com>
2020-10-10 12:03:18 +0530

    Add factory scaffold

---
+++ core/include/marlin/core/transports/TransportFactoryScaffold.hpp
@@ -0,0 +1,47 @@
+#ifndef MARLIN_CORE_TRANSPORTFACTORYSCAFFOLD
+#define MARLIN_CORE_TRANSPORTFACTORYSCAFFOLD
+
+#include <marlin/core/TransportManager.hpp>
+#include <marlin/core/SocketAddress.hpp>
+
+namespace marlin {
+namespace core {
+
+template<
+	typename TransportFactoryType,
+	typename TransportType,
+	typename ListenDelegate,
+	typename TransportDelegate,
+	typename BaseTransportFactoryType,
+	typename BaseTransportType
+>
+class TransportFactoryScaffold {
+protected:
+	BaseTransportFactoryType f;
+
+	ListenDelegate* delegate = nullptr;
+	TransportManager<TransportType> transport_manager;
+
+public:
+	template<typename ...Args>
+	TransportFactoryScaffold(Args&&... args);
+
+	// BaseTransportFactoryType delegate
+	bool should_accept(SocketAddress const& addr);
+	void did_create_transport(BaseTransportType base_transport);
+
+	core::SocketAddress addr;
+
+	int bind(SocketAddress const& addr);
+	int listen(ListenDelegate& delegate);
+	int dial(SocketAddress const& addr, ListenDelegate& delegate);
+
+	TransportType* get_transport(SocketAddress const& addr);
+};
+
+}  // namespace core
+}  // namespace marlin
+
+#include "TransportFactoryScaffold.ipp"
+
+#endif  // MARLIN_CORE_TRANSPORTFACTORYSCAFFOLD
