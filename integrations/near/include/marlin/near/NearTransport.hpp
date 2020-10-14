3336139a337ed1ad27959eac60529d0d35342b56
utsavrohilla <utsavrohillagithub@gmail.com>
2020-10-15 04:52:41 +0530

    Working buffering.

--- integrations/near/include/marlin/near/NearTransport.hpp
+++ integrations/near/include/marlin/near/NearTransport.hpp
@@ -28,20 +28,23 @@ private:
 		LengthWait,
 		EnumOptionWait,
 		HandshakeReading,
+		BlockReading,
 		Done,
 		NotHandled
 	};
-	State state = State::Idle;
+	State state = State::LengthWait;
 
 	uint8_t *buf; // The limit is yet to be set.
 	uint32_t buf_size = 0;
-	uint32_t bytes_remaining = 0;
+	uint32_t total_size = 0;
+	uint32_t bytes_remaining = 4;
 public:
 	DelegateType *delegate;
 	void did_recv_bytes(BaseTransport &transport, core::Buffer &&bytes);
 	void did_dial(BaseTransport &transport);
 	void did_send_bytes(BaseTransport &transport, core::Buffer &&bytes);
 	void did_close(BaseTransport &transport, uint16_t reason);
+	void send(core::Buffer &&message);
 
 	NearTransport(
 		core::SocketAddress const &src_addr,
@@ -86,199 +89,84 @@ void NearTransport<DelegateType>::setup(
 	transport.setup(this);
 }
 
+template<typename DelegateType>
+void NearTransport<DelegateType>::send(
+	core::Buffer &&message
+) {
+	transport.send(std::move(message));
+}
+
 
 template <typename DelegateType>
 void NearTransport<DelegateType>::did_recv_bytes(
 	BaseTransport&,
 	core::Buffer &&bytes
 ) {
-	// if(bytes_remaining > bytes.size()) { // Partial message
-	// 	SPDLOG_INFO("Partial: {}, {}, {}", buf_size, bytes.size(), bytes_remaining);
-	// 	std::memcpy(buf + buf_size, bytes.data(), bytes.size());
-	// 	buf_size += bytes.size();
-	// 	bytes_remaining -= bytes.size();
-	// } else { // Full message
-		SPDLOG_INFO("Full: {}, {}, {}, {}", buf_size, bytes.size(), bytes_remaining, state);
-		SPDLOG_INFO(spdlog::to_hex(bytes.data(), bytes.data() + bytes.size()));
-		// std::memcpy(buf + buf_size, bytes.data(), bytes_remaining);
-		// buf_size += bytes_remaining;
-		// bytes.cover_unsafe(bytes_remaining);
-		// bytes_remaining = 0;
-
-	if(state == State::Idle) {
-		state = State::LengthWait;
-	} else if(state == State::LengthWait) {
-		for(int i = 3; i >= 0; i--) {
-			bytes_remaining = bytes.data() [i] + (bytes_remaining << 8);
-		}
-		std::memcpy(buf + buf_size, bytes.data(), 4);
-		buf_size += 4;
-		bytes.cover_unsafe(4);
-		state = State::EnumOptionWait;
-	} else if(state == State::EnumOptionWait) {
-		uint8_t enum_opt = bytes.data()[0];
-		SPDLOG_INFO("{}", enum_opt);
-		if(enum_opt == 16) {
-			state = State::HandshakeReading;
-		} else {
-			state = State::NotHandled;
-		}
-		std::memcpy(buf + buf_size, bytes.data(), 1);
-		bytes_remaining--;
-		buf_size++;
-		bytes.cover_unsafe(1);
-	} else if(state == State::HandshakeReading) {
+	// SPDLOG_INFO("{}", spdlog::to_hex(bytes.data(), bytes.data() + bytes.size()));
+	if(bytes_remaining > bytes.size()) { // Partial message
+		SPDLOG_INFO("Partial: {}, {}, {}", buf_size, bytes.size(), bytes_remaining);
 		std::memcpy(buf + buf_size, bytes.data(), bytes.size());
 		buf_size += bytes.size();
 		bytes_remaining -= bytes.size();
 		bytes.cover_unsafe(bytes.size());
-		if(bytes_remaining == 0) {
-			state = State::Done;
-		}
-	} else if(state == State::NotHandled) {
-		SPDLOG_INFO("Kuch to gadbad hai");
-		return;
-	}
+	} else { // Full message
+		SPDLOG_INFO("Full: {}, {}, {}, {}, {}", buf_size, bytes.size(), bytes_remaining, state, total_size);
+		std::memcpy(buf + buf_size, bytes.data(), bytes_remaining);
+		buf_size += bytes_remaining;
+		bytes.cover_unsafe(bytes_remaining);
+		bytes_remaining = 0;
 
-	if(bytes.size() > 0) {
-		did_recv_bytes(transport, std::move(bytes));
-	} else {
-		// delegate->did_recv_message(*this, std::move(bytes));
-		if(state == State::Done) {
-			state = State::Idle;
-			reply_handshake();
-			SPDLOG_INFO("smthsmth");
-			SPDLOG_INFO(spdlog::to_hex(buf, buf + buf_size));
-		}
-	}
-	// }
-}
-
-// template <typename DelegateType>
-// void NearTransport<DelegateType>::did_recv_bytes(
-// 	BaseTransport&,
-// 	core::Buffer &&bytes
-// ) {
-// 	// SPDLOG_INFO(
-// 	// 	"message aaya: {}",
-// 	// 	spdlog::to_hex(bytes.data(), bytes.data() + bytes.size())
-
-// 	// );
-// 	SPDLOG_INFO("Got something");
-// 	std::memcpy(buf + buf_size, bytes.data(), bytes.size());
-// 	buf_size += bytes.size();
-
-// 	if(state == State::Idle) {
-// 		// remaining 4
-// 		// handle LengthWait
-// 		uint8_t *buf = bytes.data();
-// 		bytes_remaining = buf[3];
-// 		bytes_remaining = buf[2] + (bytes_remaining << 8);
-// 		bytes_remaining = buf[1] + (bytes_remaining << 8);
-// 		bytes_remaining = buf[0] + (bytes_remaining << 8);
-// 		bytes_remaining += 4;
-// 		// Partial handshake
-// 		// bytes.cover_unsafe(4);
-// 		state = State::EnumOptionWait;
-// 	}
-
-// 	// Idle -> LengthWait -> HandshakeWait ---- Have entire handshake
-// 	if(state == State::EnumOptionWait) {
-// 		uint8_t option = bytes.read_uint8_unsafe(4);
-// 		if(option == 16) {
-// 			state = State::HandshakeReading;
-// 		} else {
-// 			// Handle this case.
-// 		}
-// 	}
-// 	SPDLOG_INFO("message wapas bhejing1");
-// 	if(state == State::HandshakeReading) {
-// 		bytes_remaining -= bytes.size();
-// 		if(bytes_remaining == 0 and buf_size > 0) {
-// 			reply_handshake();
-// 			state = State::Idle;
-// 			buf_size = 0;
-// 		}
-// 	}
-// 	SPDLOG_INFO("message receive waala");
-// 	delegate->did_recv_message(*this, std::move(bytes));
-// 	SPDLOG_INFO("smth");
-// }
-
-
-
-
-
-template <typename DelegateType>
-void NearTransport<DelegateType>::reply_handshake() {
-	SPDLOG_INFO("Replying");
-	uint8_t near_node_key[33], gateway_key[33];
-	std::memcpy(near_node_key, buf + 13, 33);
-	std::memcpy(gateway_key, buf + 46, 33);
-	std::memcpy(buf + 13, gateway_key, 33);
-	std::memcpy(buf + 46, near_node_key, 33);
-
-	uint8_t near_node_signature[crypto_sign_BYTES];
-	memcpy(near_node_signature, buf + buf_size - crypto_sign_BYTES, crypto_sign_BYTES); // copy last crypto_sign_BYTES
-
-	uint8_t msg_sig[74];
-	bool flag = true; // true if nearkey < gatewayKey
-	for(int i = 0; i < 33; i++) {
-		if(near_node_key[i] == gateway_key[i]) {
-			// nothing
-		} else {
-			if(near_node_key[i] > gateway_key[i]) {
-				flag = false;
-			} else {
-				flag = true;
+		if(state == State::Idle) {
+			bytes_remaining = 4;
+			state = State::LengthWait;
+		} else if(state == State::LengthWait) {
+			for(int i = 3; i >= 0; i--) {
+				total_size = buf[i] + (total_size << 8);
+				SPDLOG_INFO("{}: {}", i, buf[i]);
 			}
-			break;
+			total_size += 4;
+			bytes_remaining = 1;
+			state = State::EnumOptionWait;
+		} else if(state == State::EnumOptionWait) {
+			uint8_t enum_opt = buf[4];
+			SPDLOG_INFO("{}", enum_opt);
+			if(enum_opt == 0x10) {
+				state = State::HandshakeReading;
+			} else if(enum_opt == 0xb) {
+				state = State::BlockReading;
+			} else {
+				state = State::NotHandled;
+			}
+			bytes_remaining = total_size - buf_size;
+		} else if(state == State::HandshakeReading) {
+			if(bytes_remaining == 0) {
+				state = State::Done;
+			}
+		} else if(state == State::BlockReading) {
+			if(bytes_remaining == 0) {
+				state = State::Done;
+			}
+		} else if(state == State::NotHandled) {
+			SPDLOG_INFO("Kuch to gadbad hai");
+			if(bytes_remaining == 0) {
+				state = State::Done;
+			}
+			// return;
+		}
+
+		if(bytes_remaining == 0) {
+			uint8_t *cur_msg = new uint8_t[buf_size];
+			std::memcpy(cur_msg, buf, buf_size);
+			core::Buffer message(cur_msg, buf_size);
+			delegate->did_recv_message(*this, std::move(message));
+			state = State::LengthWait;
+			bytes_remaining = 4;
+			buf_size = total_size = 0;
+		}
+		if(bytes.size() > 0) {
+			did_recv_bytes(transport, std::move(bytes));
 		}
 	}
-	if(flag) {
-		memcpy(msg_sig, near_node_key, 33);
-		memcpy(msg_sig + 33, gateway_key, 33);
-	} else {
-		memcpy(msg_sig, gateway_key, 33);
-		memcpy(msg_sig + 33, near_node_key, 33);
-	}
-	memcpy(msg_sig + 66, buf + buf_size - 73, 8);
-	SPDLOG_INFO(
-		"Signature message: {}",
-		spdlog::to_hex(msg_sig, msg_sig + 74)
-	);
-// Hash the message
-	using namespace CryptoPP;
-	SHA256 sha256;
-	uint8_t hashed_message[32];
-	sha256.Update(msg_sig, 74);
-	sha256.TruncatedFinal(hashed_message, 32);
-
-	if(crypto_sign_verify_detached(near_node_signature, hashed_message, 32, near_node_key + 1) != 0) {
-		SPDLOG_INFO("Signature verification failed");
-	} else {
-		SPDLOG_INFO("Signature verified successfully");
-	}
-
-	uint8_t myPrivateKey[crypto_sign_SECRETKEYBYTES];
-	memcpy(myPrivateKey, delegate->static_sk, crypto_sign_SECRETKEYBYTES);
-		SPDLOG_INFO("{}", spdlog::to_hex(myPrivateKey, myPrivateKey + 64));
-		SPDLOG_INFO("{}", spdlog::to_hex(hashed_message, hashed_message + 32));
-	uint8_t mySignature[64];
-	crypto_sign_detached(mySignature, NULL, hashed_message, 32, myPrivateKey);
-		// if(crypto_sign_verify_detached(mySignature, hashed_message, 32, delegate->static_pk) != 0) {
-		// 	SPDLOG_INFO("BAD");
-		// } else {
-		// 	SPDLOG_INFO("GOOD");
-		// }
-	memcpy(buf + buf_size - 64, mySignature, 64);
-
-	core::Buffer message(buf, buf_size);
-	SPDLOG_INFO(
-		"Message replying to NearTransport: {}",
-		spdlog::to_hex(message.data(), message.data() + message.size())
-	);
-	transport.send(std::move(message));
 }
 
 }
--- integrations/near/include/marlin/near/OnRampNear.hpp
+++ integrations/near/include/marlin/near/OnRampNear.hpp
@@ -14,10 +14,12 @@ static const char b58digits_ordered[] = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdef
 
 class OnRampNear {
 public:
+
 	uint8_t static_sk[crypto_sign_SECRETKEYBYTES], static_pk[crypto_sign_PUBLICKEYBYTES];
 	NearTransport <OnRampNear> *nearTransport = nullptr;
 	NearTransportFactory <OnRampNear, OnRampNear> f;
 
+	void reply_handshake(core::Buffer &&message);
 
 	OnRampNear() {
 		if(sodium_init() == -1) {
@@ -59,14 +61,14 @@ public:
 			++zcount;
 
 		size = (binsz - zcount) * 138 / 100 + 1;
-		uint8_t *buf = new uint8_t[size];
-		memset(buf, 0, size);
+		uint8_t *message = new uint8_t[size];
+		memset(message, 0, size);
 		for (i = zcount, high = size - 1; i < binsz; ++i, high = j)
 		{
 			for (carry = bin[i], j = size - 1; (j > high) || carry; --j)
 			{
-				carry += 256 * buf[j];
-				buf[j] = carry % 58;
+				carry += 256 * message[j];
+				message[j] = carry % 58;
 				carry /= 58;
 				if (!j) {
 					// Otherwise j wraps to maxint which is > high
@@ -74,7 +76,7 @@ public:
 				}
 			}
 		}
-		for (j = 0; j < size && !buf[j]; ++j);
+		for (j = 0; j < size && !message[j]; ++j);
 
 		if (*b58sz <= zcount + size - j)
 		{
@@ -86,7 +88,7 @@ public:
 		if (zcount)
 			memset(b58, '1', zcount);
 		for (i = zcount; j < size; ++i, ++j)
-			b58[i] = b58digits_ordered[buf[j]];
+			b58[i] = b58digits_ordered[message[j]];
 		b58[i] = '\0';
 		*b58sz = i + 1;
 
@@ -99,6 +101,9 @@ public:
 			message.size(),
 			spdlog::to_hex(message.data(), message.data() + message.size())
 		);
+		if(message.data()[4] == 0x10) {
+			reply_handshake(std::move(message));
+		}
 	}
 
 	void did_send_message(NearTransport<OnRampNear> &, Buffer &&message) {
@@ -126,6 +131,74 @@ public:
 		transport.setup(this);
 	}
 };
+
+
+void OnRampNear::reply_handshake(core::Buffer &&message) {
+	SPDLOG_INFO("Replying");
+	uint8_t *buf = message.data();
+	uint32_t buf_size = message.size();
+	uint8_t near_node_key[33], gateway_key[33];
+	std::memcpy(near_node_key, buf + 13, 33);
+	std::memcpy(gateway_key, buf + 46, 33);
+	std::memcpy(buf + 13, gateway_key, 33);
+	std::memcpy(buf + 46, near_node_key, 33);
+
+	uint8_t near_node_signature[crypto_sign_BYTES];
+	memcpy(near_node_signature, buf + buf_size - crypto_sign_BYTES, crypto_sign_BYTES); // copy last crypto_sign_BYTES
+
+	uint8_t msg_sig[74];
+	bool flag = true; // true if nearkey < gatewayKey
+	for(int i = 0; i < 33; i++) {
+		if(near_node_key[i] == gateway_key[i]) {
+			// nothing
+		} else {
+			if(near_node_key[i] > gateway_key[i]) {
+				flag = false;
+			} else {
+				flag = true;
+			}
+			break;
+		}
+	}
+	if(flag) {
+		memcpy(msg_sig, near_node_key, 33);
+		memcpy(msg_sig + 33, gateway_key, 33);
+	} else {
+		memcpy(msg_sig, gateway_key, 33);
+		memcpy(msg_sig + 33, near_node_key, 33);
+	}
+	memcpy(msg_sig + 66, buf + buf_size - 73, 8);
+	SPDLOG_INFO(
+		"Signature buf: {}",
+		spdlog::to_hex(msg_sig, msg_sig + 74)
+	);
+// Hash the buf
+	using namespace CryptoPP;
+	SHA256 sha256;
+	uint8_t hashed_message[32];
+	sha256.Update(msg_sig, 74);
+	sha256.TruncatedFinal(hashed_message, 32);
+
+	if(crypto_sign_verify_detached(near_node_signature, hashed_message, 32, near_node_key + 1) != 0) {
+		SPDLOG_INFO("Signature verification failed");
+	} else {
+		SPDLOG_INFO("Signature verified successfully");
+	}
+
+		// SPDLOG_INFO("{}", spdlog::to_hex(myPrivateKey, myPrivateKey + 64));
+		// SPDLOG_INFO("{}", spdlog::to_hex(hashed_message, hashed_message + 32));
+	uint8_t mySignature[64];
+	crypto_sign_detached(mySignature, NULL, hashed_message, 32, static_sk);
+		// if(crypto_sign_verify_detached(mySignature, hashed_message, 32, delegate->static_pk) != 0) {
+		// 	SPDLOG_INFO("BAD");
+		// } else {
+		// 	SPDLOG_INFO("GOOD");
+		// }
+	memcpy(buf + buf_size - 64, mySignature, 64);
+
+	nearTransport->send(std::move(message));
+}
+
 
 }
 }
