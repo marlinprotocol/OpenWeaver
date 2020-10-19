915f68dc575ff2a8cfc0b6a0e8ba109403387c14
utsavrohilla <utsavrohillagithub@gmail.com>
2020-10-20 02:36:45 +0530

    Block processing.

--- integrations/near/examples/near.cpp
+++ integrations/near/examples/near.cpp
@@ -6,12 +6,23 @@ using namespace marlin::core;
 using namespace marlin::asyncio;
 
 int main() {
+	std::string beacon_addr = "0.0.0.0:90002";
+	std::string discovery_addr = "0.0.0.0:15002";
+	std::string pubsub_addr = "0.0.0.0:15000";
 
-	uint8_t static_sk[crypto_box_SECRETKEYBYTES];
-	uint8_t static_pk[crypto_box_PUBLICKEYBYTES];
-	crypto_box_keypair(static_pk, static_sk);
+	uint8_t static_sk[crypto_sign_SECRETKEYBYTES];
+	uint8_t static_pk[crypto_sign_PUBLICKEYBYTES];
+	crypto_sign_keypair(static_pk, static_sk);
+	DefaultMulticastClientOptions clop {
+		static_sk,
+		static_pk,
+		std::vector<uint16_t>({0, 1}),
+		beacon_addr,
+		discovery_addr,
+		pubsub_addr
+	};
 
-	OnRampNear onrampNear;
+	OnRampNear onrampNear(clop);
 	std::cout << "LOL\n";
 	return uv_run(uv_default_loop(), UV_RUN_DEFAULT);
 }
--- integrations/near/include/marlin/near/OnRampNear.hpp
+++ integrations/near/include/marlin/near/OnRampNear.hpp
@@ -1,11 +1,14 @@
 #ifndef MARLIN_ONRAMP_NEAR_HPP
 #define MARLIN_ONRAMP_NEAR_HPP
 
+#include <marlin/multicast/DefaultMulticastClient.hpp>
 #include <marlin/near/NearTransportFactory.hpp>
 #include <marlin/near/NearTransport.hpp>
+#include <cryptopp/blake2.h>
 
 using namespace marlin::near;
 using namespace marlin::core;
+using namespace marlin::multicast;
 
 namespace marlin {
 namespace near {
@@ -14,28 +17,30 @@ static const char b58digits_ordered[] = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdef
 
 class OnRampNear {
 public:
-
-	uint8_t static_sk[crypto_sign_SECRETKEYBYTES], static_pk[crypto_sign_PUBLICKEYBYTES];
+	DefaultMulticastClient<OnRampNear> multicastClient;
+	uint8_t static_sk[crypto_sign_SECRETKEYBYTES], static_pk[crypto_sign_PUBLICKEYBYTES]; // should be moved to DefaultMulticastClient and add a function there to sign a message.
 	NearTransport <OnRampNear> *nearTransport = nullptr;
 	NearTransportFactory <OnRampNear, OnRampNear> f;
 
-	void reply_handshake(core::Buffer &&message);
+	void handle_handshake(core::Buffer &&message);
+	void handle_transaction(core::Buffer &&message);
+	void handle_block(core::Buffer &&message);
 
-	OnRampNear() {
+	OnRampNear(DefaultMulticastClientOptions clop): multicastClient(clop) {
+		multicastClient.delegate = this;
+		memcpy(static_sk, clop.static_sk, crypto_sign_SECRETKEYBYTES);
+		memcpy(static_pk, clop.static_pk, crypto_sign_PUBLICKEYBYTES);
 		if(sodium_init() == -1) {
 			throw;
 		}
 
-		crypto_sign_keypair(static_pk, static_sk);
-		size_t *afterSize = (size_t*)malloc(10);
-		*afterSize = (size_t)100;
 		char *b58 = (char*)std::malloc(100);
 		SPDLOG_INFO(
 			"PrivateKey: {} \n PublicKey: {}",
-			spdlog::to_hex(static_sk, static_sk + 32),
-			spdlog::to_hex(static_pk, static_pk + 32)
+			spdlog::to_hex(clop.static_sk, clop.static_sk + crypto_sign_SECRETKEYBYTES),
+			spdlog::to_hex(clop.static_pk, clop.static_pk + 32)
 		);
-		if(b58enc(b58, afterSize, static_pk, 32)) {
+		if(b58enc_mine(b58, 44, clop.static_pk, 32)) {
 			printf("%s\n", b58);
 		} else {
 			printf("Failed\n");
@@ -51,47 +56,27 @@ public:
 		f.listen(*this);
 	}
 
-	bool b58enc(char *b58, size_t *b58sz, const void *data, size_t binsz) {
-		const uint8_t *bin = (uint8_t*)data;
-		int carry;
-		size_t i, j, high, zcount = 0;
-		size_t size;
 
-		while (zcount < binsz && !bin[zcount])
-			++zcount;
-
-		size = (binsz - zcount) * 138 / 100 + 1;
-		uint8_t *message = new uint8_t[size];
-		memset(message, 0, size);
-		for (i = zcount, high = size - 1; i < binsz; ++i, high = j)
-		{
-			for (carry = bin[i], j = size - 1; (j > high) || carry; --j)
-			{
-				carry += 256 * message[j];
-				message[j] = carry % 58;
+	bool b58enc_mine(char *b58, size_t b58_size, const uint8_t *data, size_t data_size) {
+		memset(b58, 0, b58_size);
+		for(size_t i = 0; i < b58_size; i++) {
+			b58[i] = 0;
+		}
+		b58[b58_size] = '\0';
+		int high = b58_size - 1;
+		for(int i = 0; i < int(data_size); i++) {
+			int j = b58_size - 1, carry = data[i];
+			while((j > high || carry > 0) && j >= 0) {
+				carry += 256 * b58[j];
+				b58[j] = carry % 58;
 				carry /= 58;
-				if (!j) {
-					// Otherwise j wraps to maxint which is > high
-					break;
-				}
+				j--;
 			}
+			high = j;
 		}
-		for (j = 0; j < size && !message[j]; ++j);
-
-		if (*b58sz <= zcount + size - j)
-		{
-			std::cout << "Yp\n";
-			*b58sz = zcount + size - j + 1;
-			return false;
+		for(int i = 0; i < int(b58_size); i++) {
+			b58[i] = b58digits_ordered[int(b58[i])];
 		}
-
-		if (zcount)
-			memset(b58, '1', zcount);
-		for (i = zcount; j < size; ++i, ++j)
-			b58[i] = b58digits_ordered[message[j]];
-		b58[i] = '\0';
-		*b58sz = i + 1;
-
 		return true;
 	}
 
@@ -102,9 +87,22 @@ public:
 			spdlog::to_hex(message.data(), message.data() + message.size())
 		);
 		if(message.data()[4] == 0x10) {
-			reply_handshake(std::move(message));
+			handle_handshake(std::move(message));
+		} else if(message.data()[4] == 0xc) {
+			handle_transaction(std::move(message));
+		} else if(message.data()[4] == 0xb) {
+			handle_block(std::move(message));
 		}
 	}
+
+	template<typename T> // TODO: Code smell, remove later
+	void did_recv_message(
+		DefaultMulticastClient<OnRampNear> &,
+		Buffer &&,
+		T,
+		uint16_t,
+		uint64_t
+	) {}
 
 	void did_send_message(NearTransport<OnRampNear> &, Buffer &&message) {
 		SPDLOG_INFO(
@@ -124,6 +122,17 @@ public:
 		return true;
 	}
 
+
+	void did_subscribe(
+		DefaultMulticastClient<OnRampNear> &,
+		uint16_t
+	) {}
+
+	void did_unsubscribe(
+		DefaultMulticastClient<OnRampNear> &,
+		uint16_t
+	) {}
+
 	void did_close(NearTransport<OnRampNear> &, uint16_t) {}
 
 	void did_create_transport(NearTransport <OnRampNear> &transport) {
@@ -133,7 +142,37 @@ public:
 };
 
 
-void OnRampNear::reply_handshake(core::Buffer &&message) {
+void OnRampNear::handle_transaction(core::Buffer &&message) {
+	SPDLOG_INFO("Handling transaction: {}", spdlog::to_hex(message.data(), message.data() + message.size()));
+
+	CryptoPP::BLAKE2b blake2b((uint)8);
+	blake2b.Update((uint8_t *)message.data(), message.size());
+	uint64_t message_id;
+	blake2b.TruncatedFinal((uint8_t *)&message_id, 8);
+	multicastClient.ps.send_message_on_channel(
+		1,
+		message_id,
+		message.data(),
+		message.size()
+	);
+}
+
+void OnRampNear::handle_block(core::Buffer &&message) {
+	SPDLOG_INFO("Handling block");
+
+	CryptoPP::BLAKE2b blake2b((uint)8);
+	blake2b.Update((uint8_t *)message.data(), message.size());
+	uint64_t message_id;
+	blake2b.TruncatedFinal((uint8_t *)&message_id, 8);
+	multicastClient.ps.send_message_on_channel(
+		0,
+		message_id,
+		message.data(),
+		message.size()
+	);
+}
+
+void OnRampNear::handle_handshake(core::Buffer &&message) {
 	SPDLOG_INFO("Replying");
 	uint8_t *buf = message.data();
 	uint32_t buf_size = message.size();
@@ -189,11 +228,11 @@ void OnRampNear::reply_handshake(core::Buffer &&message) {
 		// SPDLOG_INFO("{}", spdlog::to_hex(hashed_message, hashed_message + 32));
 	uint8_t mySignature[64];
 	crypto_sign_detached(mySignature, NULL, hashed_message, 32, static_sk);
-		// if(crypto_sign_verify_detached(mySignature, hashed_message, 32, delegate->static_pk) != 0) {
-		// 	SPDLOG_INFO("BAD");
-		// } else {
-		// 	SPDLOG_INFO("GOOD");
-		// }
+		if(crypto_sign_verify_detached(mySignature, hashed_message, 32, static_pk) != 0) {
+			SPDLOG_INFO("BAD");
+		} else {
+			SPDLOG_INFO("GOOD");
+		}
 	memcpy(buf + buf_size - 64, mySignature, 64);
 
 	nearTransport->send(std::move(message));
