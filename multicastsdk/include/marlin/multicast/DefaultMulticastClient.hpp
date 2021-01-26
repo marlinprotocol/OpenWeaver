3a61a14d3d8d0757ac81eb693310e5a4071ca08e
Roshan Raghupathy <thisisroshansmail@gmail.com>
2021-01-26 15:02:27 +0530

    disable connection logs

--- multicastsdk/include/marlin/multicast/DefaultMulticastClient.hpp
+++ multicastsdk/include/marlin/multicast/DefaultMulticastClient.hpp
@@ -142,7 +142,7 @@ public:
 		for (auto* transport [[maybe_unused]] : sol_standby_conns) {
 			auto* static_pk = transport->get_static_pk();
 			auto* remote_static_pk = transport->get_remote_static_pk();
-			SPDLOG_INFO(
+			SPDLOG_DEBUG(
 				"Node {:spn}: Standby conn: {:spn}: rtt: {}",
 				spdlog::to_hex(static_pk, static_pk + crypto_box_PUBLICKEYBYTES),
 				spdlog::to_hex(remote_static_pk, remote_static_pk + crypto_box_PUBLICKEYBYTES),
@@ -153,7 +153,7 @@ public:
 		for (auto* transport [[maybe_unused]] : sol_conns) {
 			auto* static_pk = transport->get_static_pk();
 			auto* remote_static_pk = transport->get_remote_static_pk();
-			SPDLOG_INFO(
+			SPDLOG_DEBUG(
 				"Node {:spn}: Sol conn: {:spn}: rtt: {}",
 				spdlog::to_hex(static_pk, static_pk + crypto_box_PUBLICKEYBYTES),
 				spdlog::to_hex(remote_static_pk, remote_static_pk + crypto_box_PUBLICKEYBYTES),
--- pubsub/include/marlin/pubsub/PubSubNode.hpp
+++ pubsub/include/marlin/pubsub/PubSubNode.hpp
@@ -188,7 +188,7 @@ private:
 		for(auto& conn : unsol_conns) {
 			auto* static_pk = conn->get_static_pk();
 			auto* remote_static_pk = conn->get_remote_static_pk();
-			SPDLOG_INFO(
+			SPDLOG_DEBUG(
 				"Node {:spn}: Unsol conn: {:spn}: rtt: {}",
 				spdlog::to_hex(static_pk, static_pk + crypto_box_PUBLICKEYBYTES),
 				spdlog::to_hex(remote_static_pk, remote_static_pk + crypto_box_PUBLICKEYBYTES),
