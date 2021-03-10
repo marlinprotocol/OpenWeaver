9c525a3d6ae4f3b3d18e8a966fe65e26d5e45061
Roshan Raghupathy <thisisroshansmail@gmail.com>
2021-03-10 18:57:15 +0530

    add udp handle

---
+++ uvpp/include/marlin/uvpp/Udp.hpp
@@ -0,0 +1,43 @@
+#ifndef MARLIN_UVPP_UDP_HPP
+#define MARLIN_UVPP_UDP_HPP
+
+#include <uv.h>
+
+#include <marlin/core/SocketAddress.hpp>
+
+
+namespace marlin {
+namespace uvpp {
+
+struct Empty {};
+
+template<typename ExtraData>
+class Udp : public uv_udp_t {
+public:
+	[[no_unique_address]] ExtraData extra_data;
+
+	// Construct data in place from arguments
+	template<typename... ExtraDataArgs>
+	Udp(ExtraDataArgs&&... extra_data_args) noexcept(noexcept(ExtraData(extra_data_args...))) : extra_data(std::forward<ExtraDataArgs>(extra_data_args)...) {}
+
+	//-------- Rule of five begin --------//
+
+	~Udp() = default;
+
+	// Not copyable
+	Udp(Udp const& udp) = delete;
+	Udp& operator=(Udp const& udp) = delete;
+
+	// Not movable
+	Udp(Udp&& udp) = delete;
+	Udp& operator=(Udp&& udp) = delete;
+
+	//-------- Rule of five end --------//
+};
+
+using UdpE = Udp<Empty>;
+
+}  // namespace uvpp
+}  // namespace marlin
+
+#endif  // MARLIN_UVPP_UDP_HPP
