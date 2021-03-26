0eecc69416613d8cf2b3a61f00ae0f44eb2d11a9
Roshan Raghupathy <thisisroshansmail@gmail.com>
2021-03-26 19:57:48 +0530

    add an example file for prototyping

---
+++ core/examples/fabric.cpp
@@ -0,0 +1,18 @@
+#include <marlin/core/fabric/Fabric.hpp>
+
+
+using namespace marlin::core;
+
+struct Fiber {
+	static constexpr bool is_outer_open = true;
+	static constexpr bool is_inner_open = true;
+
+	using InnerMessageType = int;
+	using OuterMessageType = int;
+};
+
+int main() {
+	Fabric<Fiber, Fiber, Fiber> f;
+	return 0;
+}
+
