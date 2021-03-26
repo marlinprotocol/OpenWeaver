3b578830ef4fda12e5e92b192626902021da6aad
Roshan Raghupathy <thisisroshansmail@gmail.com>
2021-03-26 18:43:09 +0530

    add concept of outer and inner message type

--- core/include/marlin/core/fabric/Fabric.hpp
+++ core/include/marlin/core/fabric/Fabric.hpp
@@ -4,12 +4,16 @@
 namespace marlin {
 namespace core {
 
+// Fibers assumed to be ordered from Outer to Inner
 template<typename... Fibers>
 class Fabric {
 
 private:
 	// Important: Not zero indexed!
 	std::tuple<void, Fibers...> fibers;
+public:
+	using OuterMessageType = decltype(std::get<0>(stages))::OuterMessageType;
+	using InnerMessageType = decltype(std::get<sizeof...(Fibers) - 1>(fibers))::InnerMessageType;
 };
 
 }  // namespace core
