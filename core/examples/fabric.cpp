c67143f4336a051f5bb74143beb2a16cfd81dfad
Roshan Raghupathy <thisisroshansmail@gmail.com>
2021-03-28 15:12:41 +0530

    use did_recv in example

--- core/examples/fabric.cpp
+++ core/examples/fabric.cpp
@@ -3,18 +3,26 @@
 
 using namespace marlin::core;
 
+template<size_t idx = 0>
 struct Fiber {
 	static constexpr bool is_outer_open = true;
 	static constexpr bool is_inner_open = true;
 
 	using InnerMessageType = Buffer;
 	using OuterMessageType = Buffer;
+
+	template<typename FabricType>
+	int did_recv(FabricType&& fabric, Buffer&& buf) {
+		SPDLOG_INFO("Did recv: {}", idx);
+		return fabric.did_recv(*this, std::move(buf));
+	}
 };
 
 int main() {
-	Fabric<Fiber, Fabric<Fiber, Fiber>, Fiber, Fiber> f;
+	Fabric<Fiber<0>, Fabric<Fiber<1>, Fiber<2>>, Fiber<3>, Fiber<4>> f;
 	// unique identity rule strikes again -_-
 	SPDLOG_INFO("{}", sizeof(f));
-	return 0;
+
+	return f.did_recv(Fiber<0>(), Buffer(5));
 }
 
