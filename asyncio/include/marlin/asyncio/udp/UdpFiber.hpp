d3e458c61377bb39af8447c8654a8824a7a3fb58
Roshan Raghupathy <thisisroshansmail@gmail.com>
2021-06-27 23:52:57 +0530

    simplify udp fiber using the scaffold

--- asyncio/include/marlin/asyncio/udp/UdpFiber.hpp
+++ asyncio/include/marlin/asyncio/udp/UdpFiber.hpp
@@ -2,6 +2,7 @@
 #define MARLIN_ASYNCIO_UDP_UDPFIBER_HPP
 
 #include <marlin/core/Buffer.hpp>
+#include <marlin/core/fibers/FiberScaffold.hpp>
 #include <marlin/uvpp/Udp.hpp>
 
 #include <spdlog/spdlog.h>
@@ -10,27 +11,29 @@ namespace marlin {
 namespace asyncio {
 
 template<typename ExtFabric>
-class UdpFiber {
+class UdpFiber : public core::FiberScaffold<
+	UdpFiber,
+	ExtFabric,
+	core::Buffer,
+	void
+> {
 public:
 	using SelfType = UdpFiber<ExtFabric>;
+	using FiberScaffoldType = core::FiberScaffold<
+		UdpFiber,
+		ExtFabric,
+		core::Buffer,
+		void
+	>;
 
-	static constexpr bool is_outer_open = false;
-	static constexpr bool is_inner_open = true;
-
-	using OuterMessageType = void;
-	using InnerMessageType = core::Buffer;
+	using typename FiberScaffoldType::InnerMessageType;
+	using typename FiberScaffoldType::OuterMessageType;
 
 private:
-	[[no_unique_address]] ExtFabric ext_fabric;
 	uvpp::UdpE* udp_handle = nullptr;
 
 public:
-	template<typename ExtTupleType>
-	UdpFiber(std::tuple<ExtTupleType>&& init_tuple) :
-		ext_fabric(std::get<0>(init_tuple)) {
-		udp_handle = new uvpp::UdpE();
-		udp_handle->data = this;
-	}
+	using FiberScaffoldType::FiberScaffoldType;
 
 	[[nodiscard]] int bind(core::SocketAddress const& addr) {
 		int res = uv_udp_init(uv_default_loop(), udp_handle);
@@ -134,13 +137,9 @@ public:
 			}
 		}
 
-		ext_fabric.i(*this).did_dial(ext_fabric.is(*this), addr, std::forward<decltype(args)>(args)...);
+		FiberScaffoldType::did_dial(*this, addr, std::forward<decltype(args)>(args)...);
 
 		return 0;
-	}
-
-	int did_recv(auto&&, core::Buffer&& buf, core::SocketAddress addr) {
-		return ext_fabric.i(*this).did_recv(ext_fabric.is(*this), std::move(buf), addr);
 	}
 
 	static void send_cb(
@@ -168,10 +167,6 @@ public:
 		}
 
 		delete req;
-	}
-
-	int did_send(auto&&, core::Buffer&& buf) {
-		return ext_fabric.i(*this).did_send(ext_fabric.is(*this), std::move(buf));
 	}
 
 	[[nodiscard]] int send(auto&&, core::Buffer&& buf, core::SocketAddress addr) {
