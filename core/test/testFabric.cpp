f86de62d08e5070b689558062d59109988a50f73
utsavrohilla <utsavrohillagithub@gmail.com>
2021-07-06 07:41:06 +0530

    Fixed merge changes.

--- core/test/testFabric.cpp
+++ core/test/testFabric.cpp
@@ -29,8 +29,7 @@ struct Terminal {
 		idx(std::get<0>(std::get<0> (init_tuple))),
 		indices(std::get<1>(std::get<0>(init_tuple))) {}
 
-	template<typename FiberType>
-	int did_recv(FiberType&, Buffer&&) {
+	int did_recv(auto&&, Buffer&&) {
 		(*indices).push_back("t_dr");
 		return 0;
 	}
@@ -73,34 +72,34 @@ struct Fiber {
 		ext_fabric(std::get<0>(init_tuple)),
 		idx(std::get<1>(init_tuple)) {}
 
-	int did_recv(Buffer&& buf) {
+	int did_recv(auto&&, Buffer&& buf) {
 		(*indices).push_back("f_dr_" + std::to_string(idx));
 		// SPDLOG_INFO("Did recv: {}", idx);
-		return ext_fabric.did_recv(*this, std::move(buf));
+		return ext_fabric.i(*this).did_recv(*this, std::move(buf));
 	}
 
-	int send(InnerMessageType &&buf, SocketAddress addr) {
+	int send(auto&&, InnerMessageType &&buf, SocketAddress addr) {
 		(*indices).push_back("f_s_" + std::to_string(idx));
-		return ext_fabric.send(*this, std::move(buf), addr);
+		return ext_fabric.o(*this).send(*this, std::move(buf), addr);
 	}
 
-	int dial(SocketAddress, auto&& ...) {
-		return 0;
-	}
+	// int dial(SocketAddress, auto&& ...) {
+	// 	return 0;
+	// }
 
-	template <typename... Args>
-	int did_dial(Args&&... args) {
-		(*indices).push_back("f_dd_" + std::to_string(idx));
-		return ext_fabric.did_dial(*this, std::forward<Args>(args)...);
-	}
+	// template <typename... Args>
+	// int did_dial(Args&&... args) {
+	// 	(*indices).push_back("f_dd_" + std::to_string(idx));
+	// 	return ext_fabric.did_dial(*this, std::forward<Args>(args)...);
+	// }
 
-	int bind(SocketAddress) {
-		return 0;
-	}
+	// int bind(SocketAddress) {
+	// 	return 0;
+	// }
 
-	int listen() {
-		return 0;
-	}
+	// int listen() {
+	// 	return 0;
+	// }
 
 };
 
@@ -129,26 +128,26 @@ struct FiberOuterClose {
 		idx(std::get<1>(init_tuple)) {}
 
 
-	template <typename... Args>
-	int did_dial(Args&&... args) {
-		(*indices).push_back("f_dd_" + std::to_string(idx));
-		return ext_fabric.did_dial(*this, std::forward<Args>(args)...);
-	}
+	// template <typename... Args>
+	// int did_dial(auto&&, Args&&... args) {
+	// 	(*indices).push_back("f_dd_" + std::to_string(idx));
+	// 	return ext_fabric.did_dial(*this, std::forward<Args>(args)...);
+	// }
 
 
 	template <typename... Args>
-	int dial(SocketAddress, Args&& ...) {
+	int dial(auto&&, SocketAddress, Args&& ...) {
 		(*indices).push_back("foc_d_" + std::to_string(idx));
 		// return ext_fabric.did_dial(*this, std::forward<Args>(args)...);
 		return 0;
 	}
 
-	int bind(SocketAddress) {
+	int bind(auto&&, SocketAddress) {
 		(*indices).push_back("foc_b_" + std::to_string(idx));
 		return 0;
 	}
 
-	int listen() {
+	int listen(auto&&) {
 		(*indices).push_back("foc_l_" + std::to_string(idx));
 		return 0;
 	}
@@ -165,7 +164,7 @@ TEST(FabricTest, MessageOrder1) {
 		std::make_tuple(std::make_tuple(-1, indices)),
 		std::make_tuple(std::make_tuple(1, indices))
 	));
-	f.did_recv(Buffer(5));
+	f.i(0).did_recv(0, Buffer(5));
 	EXPECT_EQ(*indices, std::vector <std::string> ({"f_dr_1", "t_dr"}));
 }
 
@@ -188,7 +187,7 @@ TEST(FabricTest, MessageOrder2) {
 		std::make_tuple(std::make_tuple(4, indices)),
 		std::make_tuple(std::make_tuple(5, indices))
 	));
-	f.did_recv(Buffer(5));
+	f.i(0).did_recv(0, Buffer(5));
 	EXPECT_EQ(*indices, std::vector <std::string>({"f_dr_1", "f_dr_2", "f_dr_3", "f_dr_4", "f_dr_5", "t_dr"}));
 }
 
@@ -207,7 +206,7 @@ TEST(FabricTest, MessageOrder3) {
 			std::make_tuple(2, indices)
 		))
 	));
-	f.did_recv(Buffer(5));
+	f.i(0).did_recv(0, Buffer(5));
 	EXPECT_EQ(*indices, std::vector <std::string> ({"f_dr_1", "f_dr_2", "t_dr"}));
 }
 
@@ -231,7 +230,7 @@ TEST(FabricTest, MessageOrder4) {
 		std::make_tuple(std::make_tuple(std::make_tuple(1, indices)), 
 						std::make_tuple(std::make_tuple(2, indices)))
 	));
-	f.did_recv(Buffer(5));
+	f.i(0).did_recv(0, Buffer(5));
 	EXPECT_EQ(*indices, std::vector <std::string> ({"f_dr_1", "f_dr_2", "f_dr_3", "f_dr_4", "f_dr_1", "f_dr_2", "t_dr"}));
 }
 
@@ -261,7 +260,7 @@ TEST(FabricTest, MessageOrder5) {
 						std::make_tuple(std::make_tuple(2, indices))),
 		std::make_tuple(std::make_tuple(4, indices))
 	));
-	f.did_recv(Buffer(5)); 
+	f.i(0).did_recv(0, Buffer(5)); 
 	EXPECT_EQ(*indices, std::vector <std::string> ({"f_dr_1", "f_dr_1", "f_dr_2", "f_dr_2", "f_dr_1", "f_dr_2", "f_dr_3", "f_dr_1", "f_dr_2", "f_dr_4", "t_dr"}));
 }
 
@@ -291,7 +290,7 @@ TEST(FabricTest, sendFunction) {
 						std::make_tuple(std::make_tuple(2, indices))),
 		std::make_tuple(std::make_tuple(4, indices))
 	));
-	f.send(Buffer(5), SocketAddress::from_string("0.0.0.0:3000"));
+	f.o(0).send(0, Buffer(5), SocketAddress::from_string("0.0.0.0:3000"));
 	EXPECT_EQ(*indices, std::vector <std::string> ({"f_s_4", "f_s_2", "f_s_1", "f_s_3", "f_s_2", "f_s_1", "f_s_2", "f_s_2", "f_s_1", "f_s_1", "t_s"}));
 }
 
@@ -311,17 +310,17 @@ TEST(FabricTest, dialFunction) {
 		std::make_tuple(std::make_tuple(-1, indices)),
 		// Other fibers
 		std::make_tuple(std::make_tuple(1, indices)),
-		std::make_tuple(std::make_tuple(std::make_tuple(1, indices)), 
+		std::make_tuple(std::make_tuple(std::make_tuple(1, indices)),
 						std::make_tuple(std::make_tuple(2, indices))),
 		std::make_tuple(std::make_tuple(2, indices)),
-		std::make_tuple(std::make_tuple(std::make_tuple(1, indices)), 
+		std::make_tuple(std::make_tuple(std::make_tuple(1, indices)),
 						std::make_tuple(std::make_tuple(2, indices))),
 		std::make_tuple(std::make_tuple(3, indices)),
 		std::make_tuple(std::make_tuple(std::make_tuple(1, indices)), 
 						std::make_tuple(std::make_tuple(2, indices))),
 		std::make_tuple(std::make_tuple(4, indices))
 	));
-	f.dial(SocketAddress::from_string("0.0.0.0:3000"), Buffer(5));
+	f.i(0).dial(0, SocketAddress::from_string("0.0.0.0:3000"), Buffer(5));
 	EXPECT_EQ(*indices, std::vector <std::string> ({"foc_d_1"}));
 }
 
@@ -352,7 +351,7 @@ TEST(FabricTest, bindFunction) {
 						std::make_tuple(std::make_tuple(2, indices))),
 		std::make_tuple(std::make_tuple(4, indices))
 	));
-	f.bind(SocketAddress::from_string("0.0.0.0:3000"));
+	f.i(0).bind(0, SocketAddress::from_string("0.0.0.0:3000"));
 	EXPECT_EQ(*indices, std::vector <std::string> ({"foc_b_1"}));
 }
 
@@ -382,6 +381,6 @@ TEST(FabricTest, listenFunction) {
 						std::make_tuple(std::make_tuple(2, indices))),
 		std::make_tuple(std::make_tuple(4, indices))
 	));
-	f.listen();
+	f.i(0).listen(0);
 	EXPECT_EQ(*indices, std::vector <std::string> ({"foc_l_1"}));
 }
