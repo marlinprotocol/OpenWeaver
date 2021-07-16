3501878981cbe1ba5600c03fd1426e0bef319f0d
Roshan Raghupathy <thisisroshansmail@gmail.com>
2021-07-16 15:59:46 +0530

    add some basic tests

---
+++ core/test/testSentinelFramingFiber.cpp
@@ -0,0 +1,57 @@
+#include <gtest/gtest.h>
+#include <marlin/core/fibers/SentinelFramingFiber.hpp>
+
+using namespace marlin::core;
+
+struct Source {
+	int leftover(auto&& source, auto&& buf, SocketAddress addr) {
+		return source.did_recv(*this, std::move(buf), addr);
+	}
+};
+
+struct Terminal {
+	Terminal(auto&&...) {}
+
+	auto& i(auto&&) { return *this; }
+	auto& o(auto&&) { return *this; }
+	auto& is(auto& f) { return f; }
+	auto& os(auto& f) { return f; }
+
+	std::function<int(Buffer&&, SocketAddress)> did_recv_impl;
+	int did_recv(auto&&, Buffer&& buf, SocketAddress addr) {
+		return did_recv_impl(std::move(buf), addr);
+	}
+
+	std::function<int(SocketAddress)> did_recv_sentinel_impl;
+	int did_recv_sentinel(auto&&, SocketAddress addr) {
+		return did_recv_sentinel_impl(addr);
+	}
+};
+
+TEST(SentinelFramingFiber, Constructible) {
+	SentinelFramingFiber<Terminal, '\n'> f(std::make_tuple(std::make_tuple()));
+}
+
+TEST(SentinelFramingFiber, SingleBufferNoSentinels) {
+	Source s;
+	Terminal t;
+
+	Buffer msg(5);
+	msg.write_unsafe(0, (uint8_t const*)"hello", 5);
+
+	SentinelFramingFiber<Terminal&, '\n'> f(std::forward_as_tuple(t));
+
+	size_t calls = 0;
+	t.did_recv_impl = [&](Buffer&& buf, SocketAddress addr) {
+		EXPECT_EQ(calls, 0);
+		calls++;
+
+		EXPECT_EQ(std::memcmp(buf.data(), "hello", 5), 0);
+		EXPECT_EQ(addr.to_string(), "192.168.0.1:8000");
+
+		return 0;
+	};
+	f.did_recv(s, std::move(msg), SocketAddress::from_string("192.168.0.1:8000"));
+	EXPECT_EQ(calls, 1);
+}
+
