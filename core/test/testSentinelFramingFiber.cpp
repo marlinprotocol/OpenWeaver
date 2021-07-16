8b75348f586972b89ad4a862682524c940ee2877
Roshan Raghupathy <thisisroshansmail@gmail.com>
2021-07-16 17:36:48 +0530

    test: MultipleBufferOneSentinel1

--- core/test/testSentinelFramingFiber.cpp
+++ core/test/testSentinelFramingFiber.cpp
@@ -94,3 +94,51 @@ TEST(SentinelFramingFiber, SingleBufferOneSentinel) {
 	EXPECT_EQ(sentinel_calls, 1);
 }
 
+TEST(SentinelFramingFiber, MultipleBufferOneSentinel1) {
+	Source s;
+	Terminal t;
+
+	Buffer msg1(5);
+	msg1.write_unsafe(0, (uint8_t const*)"hello", 5);
+	Buffer msg2(11);
+	msg2.write_unsafe(0, (uint8_t const*)"world\nagain", 11);
+
+	SentinelFramingFiber<Terminal&, '\n'> f(std::forward_as_tuple(t));
+
+	size_t bytes_calls = 0;
+	size_t sentinel_calls = 0;
+	t.did_recv_impl = [&](Buffer&& buf, SocketAddress addr) {
+		EXPECT_LT(bytes_calls, 3);
+		if(bytes_calls == 0) {
+			EXPECT_EQ(sentinel_calls, 0);
+			EXPECT_EQ(std::memcmp(buf.data(), "hello", 5), 0);
+			EXPECT_EQ(addr.to_string(), "192.168.0.1:8000");
+		} else if(bytes_calls == 1) {
+			EXPECT_EQ(sentinel_calls, 0);
+			EXPECT_EQ(std::memcmp(buf.data(), "world", 5), 0);
+			EXPECT_EQ(addr.to_string(), "192.168.0.1:8000");
+		} else if(bytes_calls == 2) {
+			EXPECT_EQ(sentinel_calls, 1);
+			EXPECT_EQ(std::memcmp(buf.data(), "again", 5), 0);
+			EXPECT_EQ(addr.to_string(), "192.168.0.1:8000");
+		}
+		bytes_calls++;
+
+		return 0;
+	};
+	t.did_recv_sentinel_impl = [&](SocketAddress addr) {
+		EXPECT_EQ(bytes_calls, 2);
+		EXPECT_EQ(sentinel_calls, 0);
+		sentinel_calls++;
+		EXPECT_EQ(addr.to_string(), "192.168.0.1:8000");
+
+		return 0;
+	};
+
+	f.did_recv(s, std::move(msg1), SocketAddress::from_string("192.168.0.1:8000"));
+	f.did_recv(s, std::move(msg2), SocketAddress::from_string("192.168.0.1:8000"));
+
+	EXPECT_EQ(bytes_calls, 3);
+	EXPECT_EQ(sentinel_calls, 1);
+}
+
