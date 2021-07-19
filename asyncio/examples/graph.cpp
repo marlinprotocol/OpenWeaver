c7f113b13fa6a8eb105c178a470f0c069f2b0ceb
Roshan Raghupathy <thisisroshansmail@gmail.com>
2021-07-19 19:36:34 +0530

    fix the graph example to properly read the body

--- asyncio/examples/graph.cpp
+++ asyncio/examples/graph.cpp
@@ -1,10 +1,14 @@
 #include <marlin/asyncio/core/Timer.hpp>
 #include <marlin/asyncio/tcp/TcpOutFiber.hpp>
-#include <marlin/core/fibers/FramingBaseFiber.hpp>
+#include <marlin/core/fibers/DynamicFramingFiber.hpp>
 #include <marlin/core/fibers/SentinelFramingFiber.hpp>
 #include <marlin/core/fibers/SentinelBufferFiber.hpp>
+#include <marlin/core/fibers/LengthFramingFiber.hpp>
+#include <marlin/core/fibers/LengthBufferFiber.hpp>
 #include <uv.h>
 #include <spdlog/spdlog.h>
+
+#include <string>
 
 
 using namespace marlin::core;
@@ -18,9 +22,10 @@ struct Grapher {
 	using FiberType = Fabric<
 		Grapher&,
 		TcpOutFiber,
-		FramingBaseFiber,
-		SentinelFramingFiberHelper,
-		SentinelBufferFiber
+		DynamicFramingFiberHelper<
+			FabricF<SentinelFramingFiberHelper, SentinelBufferFiber>::type,
+			FabricF<LengthFramingFiber, LengthBufferFiber>::type
+		>::type
 	>;
 
 	static constexpr bool is_outer_open = false;
@@ -37,9 +42,10 @@ struct Grapher {
 		auto& fiber = *new FiberType(std::forward_as_tuple(
 			*this,
 			std::make_tuple(),
-			std::make_tuple(),
-			std::make_tuple(),
-			std::make_tuple()
+			std::make_tuple(std::make_tuple(
+				std::make_tuple(),
+				std::make_tuple()
+			))
 		));
 		(void)fiber.i(*this).dial(dst);
 	}
@@ -49,16 +55,45 @@ struct Grapher {
 		t.template start<Grapher, &Grapher::query_cb>(0, 5000);
 	}
 
+	size_t state = 0;
+	size_t length = 0;
+
 	int did_recv(auto&& fiber, Buffer&& buf, SocketAddress addr) {
 		SPDLOG_INFO("Grapher: Did recv: {} bytes from {}: {}", buf.size(), addr.to_string(), std::string((char*)buf.data(), buf.size()));
-		fiber.o(*this).reset(1000000);
+
+		if(state == 0) {
+			// headers
+			if(buf.size() == 2) {
+				// empty
+				if(length == 0) {
+					// still do not have length
+					fiber.o(*this).close();
+					return -1;
+				}
+
+				// go to next phase
+				state = 1;
+				fiber.o(*this).template transform<1>(std::make_tuple(), std::make_tuple());
+				fiber.o(*this).reset(length);
+				return 0;
+			} else if(std::memcmp(buf.data(), u8"content-length: ", 16) == 0) {
+				// parse length
+				auto length_str = std::string((char*)buf.data()+16, buf.size() - 18);
+				length = std::stol(length_str);
+			}
+			fiber.o(*this).reset(1000);
+		} else {
+			// body
+			fiber.o(*this).close();
+		}
+
 		return 0;
 	}
 
 	template<typename FiberType>
 	int did_dial(FiberType& fiber, SocketAddress addr) {
 		SPDLOG_INFO("Grapher: Did dial: {}", addr.to_string());
-		fiber.o(*this).reset(1000000);
+		fiber.o(*this).reset(1000);
 		auto query = Buffer(283).write_unsafe(
 			0,
 			(uint8_t*)"POST /subgraphs/name/marlinprotocol/staking HTTP/1.0\r\nContent-Type: application/json\r\nContent-Length: 174\r\n\r\n{\"query\": \"query { clusters(where: {networkId: \\\"0x9bd00430e53a5999c7c603cfc04cbdaf68bdbc180f300e4a2067937f57a0534f\\\"}){clientKey, totalDelegations{token{tokenId} amount}}}\"}",
@@ -75,6 +110,7 @@ struct Grapher {
 	}
 
 	int did_close(auto& fiber) {
+		SPDLOG_INFO("Did close");
 		delete &fiber;
 		return 0;
 	}
