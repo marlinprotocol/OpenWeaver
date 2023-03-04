d8e6bd10c9ae823566b268ea0f7a28ad86624476
cabbage <cabbage@example.com>
2023-03-04 20:34:16 +0000

    payload and init support in mapping fiber

--- core/include/marlin/core/fibers/MappingFiber.hpp
+++ core/include/marlin/core/fibers/MappingFiber.hpp
@@ -11,15 +11,16 @@
 namespace marlin {
 namespace core {
 
-template<typename ExtFabric, template<typename> typename FiberTemplate, typename Key>
+
+template<typename ExtFabric, template<typename> typename FiberTemplate, typename Key, typename InitTuple = std::tuple<>>
 class MappingFiber : public FiberScaffold<
-	MappingFiber<ExtFabric, FiberTemplate, Key>,
+	MappingFiber<ExtFabric, FiberTemplate, Key, InitTuple>,
 	ExtFabric,
 	Buffer,
 	Buffer
 > {
 public:
-	using SelfType = MappingFiber<ExtFabric, FiberTemplate, Key>;
+	using SelfType = MappingFiber<ExtFabric, FiberTemplate, Key, InitTuple>;
 	using FiberScaffoldType = FiberScaffold<
 		SelfType,
 		ExtFabric,
@@ -32,20 +33,35 @@ public:
 
 	template<uint32_t tag>
 	auto outer_call(auto&&, Key key, auto&&... args) {
-		auto& fiber = *(fibers.try_emplace(key, new FiberTemplate<Shuttle>(std::tuple(Shuttle{*this}))).first->second);
+		auto& fiber = *(fibers.try_emplace(key, new FiberTemplate<Shuttle>(std::tuple(Shuttle{*this}, init_tuple))).first->second);
 
 		return fiber.template outer_call<tag>(*this, std::forward<decltype(args)>(args)..., key);
 	}
 
 	template<uint32_t tag>
 	auto inner_call(auto&&, Key key, auto&&... args) {
-		auto& fiber = *(fibers.try_emplace(key, new FiberTemplate<Shuttle>(std::tuple(Shuttle{*this}))).first->second);
+		auto& fiber = *(fibers.try_emplace(key, new FiberTemplate<Shuttle>(std::tuple(Shuttle{*this}, init_tuple))).first->second);
 
 		return fiber.template inner_call<tag>(*this, std::forward<decltype(args)>(args)..., key);
 	}
 
+	template<uint32_t tag>
+	auto outer_call(auto&&, auto&& payload, Key key, auto&&... args) {
+		auto& fiber = *(fibers.try_emplace(key, new FiberTemplate<Shuttle>(std::tuple(Shuttle{*this}, init_tuple))).first->second);
+
+		return fiber.template outer_call<tag>(*this, std::forward<decltype(payload)>(payload), std::forward<decltype(args)>(args)..., key);
+	}
+
+	template<uint32_t tag>
+	auto inner_call(auto&&, auto&& payload, Key key, auto&&... args) {
+		auto& fiber = *(fibers.try_emplace(key, new FiberTemplate<Shuttle>(std::tuple(Shuttle{*this}, init_tuple))).first->second);
+
+		return fiber.template inner_call<tag>(*this, std::forward<decltype(payload)>(payload), std::forward<decltype(args)>(args)..., key);
+	}
+
 	MappingFiber(auto&& init_tuple) :
-		FiberScaffoldType(std::forward_as_tuple(std::get<0>(init_tuple))) {}
+		FiberScaffoldType(std::forward_as_tuple(std::get<0>(init_tuple))),
+		init_tuple(std::get<1>(init_tuple)){}
 
 private:
 	struct Shuttle {
@@ -54,23 +70,24 @@ private:
 		Shuttle(SelfType& fabric) : fabric(fabric) {}
 
 		template<uint32_t tag>
-		auto outer_call(auto&&, Key key, auto&&... args) {
-			return fabric.ext_fabric.template outer_call<tag>(fabric, std::forward<decltype(args)>(args)..., key);
+		auto outer_call(auto&&, auto&&... args) {
+			return fabric.ext_fabric.template outer_call<tag>(fabric, std::forward<decltype(args)>(args)...);
 		}
 
 		template<uint32_t tag>
-		auto inner_call(auto&&, Key key, auto&&... args) {
-			return fabric.ext_fabric.template inner_call<tag>(fabric, std::forward<decltype(args)>(args)..., key);
+		auto inner_call(auto&&, auto&&... args) {
+			return fabric.ext_fabric.template inner_call<tag>(fabric, std::forward<decltype(args)>(args)...);
 		}
 	};
 	absl::flat_hash_map<Key, std::unique_ptr<FiberTemplate<Shuttle>>> fibers;
+	[[no_unique_address]] InitTuple init_tuple;
 };
 
 
-template<template<typename> typename FT, typename Key>
+template<template<typename> typename FT, typename Key, typename IT = std::tuple<>>
 struct MappingFiberF {
 	template<typename T>
-	using type = MappingFiber<T, FT, Key>;
+	using type = MappingFiber<T, FT, Key, IT>;
 };
 
 }  // namespace core
